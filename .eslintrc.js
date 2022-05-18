652433035b0caa0f2f2538ebf547333146c4fd37
Gig Ashton <gig@greenriver.org>
2022-05-18 13:08:45 -0400

    Prettier precommit, update eslint to remove redunant rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
   },
-  extends: [
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
-    'plugin:import/recommended'
-  ],
-  plugins: ['react', '@typescript-eslint'],
+  extends: ['airbnb/hooks', 'plugin:import/recommended', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'jest'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -20,14 +12,60 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
   },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tx'],
+      env: { 'jest/globals': true },
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'airbnb-typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['error'],
+        // Disable rules that are redundant with prettier (found with eslint-config-prettier)
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/brace-style': 'off',
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/brace-style': 'off',
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/comma-spacing': 'off',
+        '@typescript-eslint/func-call-spacing': 'off',
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/keyword-spacing': 'off',
+        '@typescript-eslint/no-extra-semi': 'off',
+        '@typescript-eslint/object-curly-spacing': 'off',
+        '@typescript-eslint/semi': 'off',
+        '@typescript-eslint/space-before-blocks': 'off',
+        '@typescript-eslint/space-before-function-paren': 'off',
+        '@typescript-eslint/space-infix-ops': 'off',
+        '@typescript-eslint/quotes': 'off',
+      },
+    },
+  ],
   rules: {
-    'linebreak-style': ['error', 'unix'],
     'import/order': [
       'error',
       {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
         'newlines-between': 'always',
         alphabetize: { order: 'asc', caseInsensitive: true },
       },
@@ -35,9 +73,5 @@ module.exports = {
     'import/default': 'off',
     'import/no-named-as-default-member': 'off',
     'import/no-named-as-default': 'off',
-    
-    '@typescript-eslint/no-unused-vars': ['error'],
-
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
   },
 };
