bbb4f129c15105575320711ef048747bf7678116
Gig Ashton <gig@greenriver.org>
2022-05-19 15:54:35 -0400

    Use https for dev servery

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,43 @@
 import { execSync } from 'child_process';
+import fs from 'fs';
 import { resolve } from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
+const setGitVariables = (process) => {
+  const commitDate = execSync('git log -1 --format=%cI').toString().trimEnd();
+  const branchName = execSync('git rev-parse --abbrev-ref HEAD')
+    .toString()
+    .trimEnd();
+  const commitHash = execSync('git rev-parse --short HEAD')
+    .toString()
+    .trimEnd();
+
+  process.env.VITE_GIT_COMMIT_DATE = commitDate;
+  process.env.VITE_GIT_BRANCH_NAME = branchName;
+  process.env.VITE_GIT_COMMIT_HASH = commitHash;
+};
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   if (mode !== 'production') {
-    const commitDate = execSync('git log -1 --format=%cI').toString().trimEnd();
-    const branchName = execSync('git rev-parse --abbrev-ref HEAD')
-      .toString()
-      .trimEnd();
-    const commitHash = execSync('git rev-parse --short HEAD')
-      .toString()
-      .trimEnd();
-
-    process.env.VITE_GIT_COMMIT_DATE = commitDate;
-    process.env.VITE_GIT_BRANCH_NAME = branchName;
-    process.env.VITE_GIT_COMMIT_HASH = commitHash;
+    setGitVariables(process);
   }
 
+  const isDev = mode === 'development';
+
   return {
+    // base: 'https://hmis.dev.test/',
+    server: {
+      open: true,
+      host: 'hmis.dev.test',
+      https: {
+        key: fs.readFileSync('.cert/key.pem'),
+        cert: fs.readFileSync('.cert/cert.pem'),
+      },
+    },
     resolve: {
       alias: {
         '@': resolve(__dirname, './src'),
