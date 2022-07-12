aeac31004aa45e8858ce9a53f02014c799977a53
Gig Ashton <gig@greenriver.org>
2022-07-12 12:16:27 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import StagingAppBar from './components/layout/StagingAppBar';
+// import StagingAppBar from './components/layout/StagingAppBar';
 
 import { AppProvider } from '@/providers/app';
 import { AppRoutes } from '@/routes';
 
 const App = () => (
   <AppProvider>
-    {import.meta.env.DEV && <StagingAppBar />}
+    {/* {import.meta.env.DEV && <StagingAppBar />} */}
     <AppRoutes />
   </AppProvider>
 );
