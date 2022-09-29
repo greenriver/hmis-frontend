ee379f8b940f1c6671dd885b4c889beafe4d1f3a
Gig Ashton <gig@greenriver.org>
2022-09-29 19:05:24 -0400

    Use accordion for all project view

--- src/components/pages/AllProjects.tsx
+++ src/components/pages/AllProjects.tsx
@@ -1,17 +1,30 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 
+import Loading from '../elements/Loading';
 import PageHeader from '../layout/PageHeader';
 
 import GroupedProjectTable from '@/modules/inventory/components/GroupedProjectTable';
+import { useGetOrganizationsAndProjectsQuery } from '@/types/gqlTypes';
 
 const AllProjects = () => {
+  const { data, loading, error } = useGetOrganizationsAndProjectsQuery();
+
+  if (loading) return <Loading />;
+  if (error) throw error;
+
   return (
     <>
       <PageHeader>
         <Typography variant='h4'>Projects</Typography>
       </PageHeader>
       <Container maxWidth='lg' sx={{ pt: 3, pb: 6 }}>
-        <GroupedProjectTable />
+        <Grid container>
+          <Grid item xs={10}>
+            {data?.organizations && (
+              <GroupedProjectTable organizations={data?.organizations} />
+            )}
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
--- src/modules/inventory/components/GroupedProjectTable.tsx
+++ src/modules/inventory/components/GroupedProjectTable.tsx
@@ -1,5 +1,13 @@
-import { Paper, Stack, Typography, Link } from '@mui/material';
-import { useCallback } from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { useCallback, useState } from 'react';
 import {
   generatePath,
   Link as RouterLink,
@@ -7,27 +15,67 @@ import {
 } from 'react-router-dom';
 
 import GenericTable, { ColumnDef } from '@/components/elements/GenericTable';
-import Loading from '@/components/elements/Loading';
 import { Routes } from '@/routes/routes';
 import { HmisEnums } from '@/types/gqlEnums';
 import {
+  GetOrganizationsAndProjectsQuery,
   ProjectFieldsFragment,
-  useGetOrganizationsAndProjectsQuery,
 } from '@/types/gqlTypes';
 
-const GroupedProjectTable = () => {
+const GroupedProjectTable = ({
+  organizations,
+}: {
+  organizations: GetOrganizationsAndProjectsQuery['organizations'];
+}) => {
   const navigate = useNavigate();
-  const { data, loading, error } = useGetOrganizationsAndProjectsQuery();
+
   const columns: ColumnDef<ProjectFieldsFragment>[] = [
-    { header: '', key: 'name', width: '30%', render: 'projectName' },
+    { header: 'Project', key: 'name', width: '10%', render: 'projectName' },
     {
-      header: '',
+      header: 'Project Type',
       key: 'type',
       width: '20%',
       render: (p: ProjectFieldsFragment) =>
         p.projectType ? HmisEnums.ProjectType[p.projectType] : null,
     },
+    // {
+    //   header: '',
+    //   key: 'view',
+    //   width: '10%',
+    //   render: (p: ProjectFieldsFragment) => (
+    //     <Button
+    //       variant='outlined'
+    //       color='secondary'
+    //       size='small'
+    //       to={generatePath(Routes.PROJECT, {
+    //         projectId: p.id,
+    //       })}
+    //       component={RouterLink}
+    //       sx={{ py: 0, float: 'right' }}
+    //     >
+    //       View Project
+    //     </Button>
+    //   ),
+    // },
   ];
+
+  const [expanded, setExpanded] = useState<Record<string, boolean>>(
+    organizations.length < 6
+      ? Object.fromEntries(organizations.map(({ id }) => [id, true]))
+      : {}
+  );
+
+  const handleChange = (id: string) => () => {
+    setExpanded((current) => {
+      const copy = { ...current };
+      if (id in copy) {
+        delete copy[id];
+      } else {
+        copy[id] = true;
+      }
+      return copy;
+    });
+  };
 
   const handleRowClick = useCallback(
     (project: ProjectFieldsFragment) =>
@@ -39,35 +87,83 @@ const GroupedProjectTable = () => {
     [navigate]
   );
 
-  if (loading) return <Loading />;
-  if (error) throw error;
-
   return (
     <>
-      {data?.organizations?.map((organization) => (
-        <Paper sx={{ p: 2, mb: 2 }} key={organization.id}>
-          <Typography variant='h6' sx={{ mb: 2 }}>
-            <Stack spacing={1} direction='row'>
-              <Link
-                variant='h6'
-                to={generatePath(Routes.ORGANIZATION, {
-                  organizationId: organization.id,
-                })}
-                component={RouterLink}
-              >
+      {organizations.map((organization) => (
+        <Accordion
+          expanded={organization.id in expanded}
+          onChange={handleChange(organization.id)}
+          key={organization.id}
+          sx={{
+            mb: 1,
+            '&.Mui-expanded': {
+              my: 1,
+            },
+            '&::before': {
+              height: '0px',
+            },
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel-${organization.id}-content`}
+            id={`panel-${organization.id}-header`}
+            sx={{
+              minHeight: '36px',
+              '.MuiAccordionSummary-content': {
+                my: 1,
+                '&.Mui-expanded': {
+                  my: 1,
+                },
+              },
+              '&.Mui-expanded': {
+                minHeight: '36px',
+                my: 0,
+              },
+            }}
+          >
+            <Stack
+              justifyContent='space-between'
+              direction='row'
+              sx={{ width: '100%', pr: 2 }}
+            >
+              <Typography variant='body1'>
                 {organization.organizationName}
-              </Link>
-              <span>-</span>
-              <span>{organization.projects.length} projects</span>
+              </Typography>
+
+              <Typography sx={{ color: 'text.secondary' }} variant='body2'>
+                {organization.projects.length} project
+                {organization.projects.length !== 1 ? 's' : ''}
+              </Typography>
             </Stack>
-          </Typography>
-          <GenericTable<ProjectFieldsFragment>
-            key={organization.id}
-            columns={columns}
-            rows={organization.projects}
-            handleRowClick={handleRowClick}
-          />
-        </Paper>
+          </AccordionSummary>
+          <AccordionDetails
+            sx={{
+              px: 0,
+              py: 1,
+              borderTop: (theme) => `1px solid ${theme.palette.grey[300]}`,
+            }}
+          >
+            <Button
+              variant='outlined'
+              color='secondary'
+              size='small'
+              to={generatePath(Routes.ORGANIZATION, {
+                organizationId: organization.id,
+              })}
+              component={RouterLink}
+              sx={{ mx: 1, my: 1, float: 'right' }}
+            >
+              View Organization
+            </Button>
+            <GenericTable<ProjectFieldsFragment>
+              key={organization.id}
+              columns={columns}
+              rows={organization.projects}
+              handleRowClick={handleRowClick}
+            />
+          </AccordionDetails>
+        </Accordion>
       ))}
     </>
   );
