ae7000ab332db1c55e27c1c81902150b98c615ec
Gig Ashton <gig@greenriver.org>
2022-10-12 13:41:57 -0400

    Remove unused

--- src/modules/auth/api/storage.ts
+++ src/modules/auth/api/storage.ts
@@ -1,5 +1,4 @@
 export const USER_STORAGE_KEY = '_hmis_user_info';
-const PREVIOUS_ROUTE = '_hmis_previous_route';
 
 // Stores user name and email. No sensitive information stored!
 export const setUser = (user: object) =>
@@ -8,12 +7,3 @@ export const setUser = (user: object) =>
 export const getUser = () => localStorage.getItem(USER_STORAGE_KEY);
 
 export const removeUser = () => localStorage.removeItem(USER_STORAGE_KEY);
-
-// Store URL before logging out
-export const setPreviousRoute = (route: string) =>
-  localStorage.setItem(PREVIOUS_ROUTE, JSON.stringify(route));
-
-export const getPreviousRoute = () => localStorage.getItem(PREVIOUS_ROUTE);
-
-export const removePreviousRoute = () =>
-  localStorage.removeItem(PREVIOUS_ROUTE);
