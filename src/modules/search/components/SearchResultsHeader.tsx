19aca189dcd9c9f1892fa9de31e0b6d7a615e263
Gig Ashton <gig@greenriver.org>
2022-10-26 11:10:04 -0400

    Set up translations

--- src/modules/search/components/SearchResultsHeader.tsx
+++ src/modules/search/components/SearchResultsHeader.tsx
@@ -9,6 +9,7 @@ import {
   ToggleButtonGroup,
   Typography,
 } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 import ButtonLink from '@/components/elements/ButtonLink';
 import { Routes } from '@/routes/routes';
@@ -26,60 +27,63 @@ const SearchResultsHeader = ({
   disabled: boolean;
   cardsEnabled: boolean;
   onChangeCards: (event: React.MouseEvent<HTMLElement>, value: any) => void;
-}) => (
-  <Grid
-    container
-    justifyContent='space-between'
-    sx={showCardToggle ? { mb: 4 } : undefined}
-  >
-    <Grid item>
-      {showCardToggle && (
-        <ToggleButtonGroup
-          value={cardsEnabled}
-          exclusive
-          onChange={onChangeCards}
-          aria-label='results display format'
-        >
-          <ToggleButton
-            value={false}
-            aria-label='table'
-            size='small'
-            disabled={disabled}
+}) => {
+  const { t } = useTranslation();
+  return (
+    <Grid
+      container
+      justifyContent='space-between'
+      sx={showCardToggle ? { mb: 4 } : undefined}
+    >
+      <Grid item>
+        {showCardToggle && (
+          <ToggleButtonGroup
+            value={cardsEnabled}
+            exclusive
+            onChange={onChangeCards}
+            aria-label='results display format'
           >
-            <ViewHeadlineIcon />
-            <Box sx={{ pl: 0.5 }}>Table</Box>
-          </ToggleButton>
-          <ToggleButton
-            value={true}
-            aria-label='cards'
-            size='small'
-            disabled={disabled}
-          >
-            <ViewCompactIcon />
-            <Box sx={{ pl: 0.5 }}>Cards</Box>
-          </ToggleButton>
-        </ToggleButtonGroup>
-      )}
+            <ToggleButton
+              value={false}
+              aria-label='table'
+              size='small'
+              disabled={disabled}
+            >
+              <ViewHeadlineIcon />
+              <Box sx={{ pl: 0.5 }}>Table</Box>
+            </ToggleButton>
+            <ToggleButton
+              value={true}
+              aria-label='cards'
+              size='small'
+              disabled={disabled}
+            >
+              <ViewCompactIcon />
+              <Box sx={{ pl: 0.5 }}>Cards</Box>
+            </ToggleButton>
+          </ToggleButtonGroup>
+        )}
+      </Grid>
+      <Grid item>
+        <Card sx={{ pl: 2, py: 1, pr: 1 }}>
+          <Stack direction='row' spacing={3} sx={{ alignItems: 'center' }}>
+            <Typography variant='body2'>
+              {t('clientSearch.addClientPrompt')}
+            </Typography>
+            <ButtonLink
+              size='small'
+              variant='outlined'
+              color='secondary'
+              to={Routes.CREATE_CLIENT}
+              target='_blank'
+            >
+              + Add Client
+            </ButtonLink>
+          </Stack>
+        </Card>
+      </Grid>
     </Grid>
-    <Grid item>
-      <Card sx={{ pl: 2, py: 1, pr: 1 }}>
-        <Stack direction='row' spacing={3} sx={{ alignItems: 'center' }}>
-          <Typography variant='body2'>
-            Don't see the client you're looking for?
-          </Typography>
-          <ButtonLink
-            size='small'
-            variant='outlined'
-            color='secondary'
-            to={Routes.CREATE_CLIENT}
-            target='_blank'
-          >
-            + Add Client
-          </ButtonLink>
-        </Stack>
-      </Card>
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default SearchResultsHeader;
