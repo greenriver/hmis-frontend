bbf6619ecd39f57a52b15f0adbfda1fd66f722a3
Gig Ashton <gig@greenriver.org>
2022-12-14 15:29:35 -0500

    Dont wait on

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,7 @@
 describe('HMIS', () => {
   beforeEach(() => {
     cy.visit('/');
+    // TODO just intercept and pass the form defs. mock serevr is too much to deal with
   });
 
   it('search and edit client', () => {
@@ -9,13 +10,19 @@ describe('HMIS', () => {
     cy.testId('table-linkedCell').first().click();
     cy.testId('clientProfile').should('be.visible');
     cy.testId('editClientButton').first().click();
-    // ensure all values are filled in
-    // do it in a generic way and do this for all forms
+
+    // cy.testId('formField').each(($elem) => {
+    //   cy.wrap($elem).find('input').as('input');
+    //   cy.get('@input').should('have.attr', 'name');
+
+    //   // TODO need special case for multi-select which has empty value
+    //   cy.get('@input').invoke('val').should('not.be.empty');
+    // });
   });
 
   it('search and create new client', () => {
     cy.get('input[name="textSearch"]').type('ja sm{enter}');
-    cy.testId('addClientButton').first().click();
+    // cy.testId('addClientButton').first().click();
     // fill in all values
     // command-click and inspect the transformed values
   });
