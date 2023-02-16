6e22a9919b644f37389bf029b5b121e182627987
Gig Ashton <gig@greenriver.org>
2023-02-16 16:45:08 -0500

    Split out tab panel

---
+++ src/modules/assessments/components/household/HouseholdAssessmentTabPanel.tsx
@@ -0,0 +1,162 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import CheckIcon from '@mui/icons-material/Check';
+import { memo, useCallback } from 'react';
+
+import IndividualAssessment from '../IndividualAssessment';
+
+import AlwaysMountedTabPanel from './AlwaysMountedTabPanel';
+import { AssessmentStatus, TabDefinition } from './HouseholdAssessments';
+
+import { AssessmentFieldsFragment, AssessmentRole } from '@/types/gqlTypes';
+
+const tabPanelA11yProps = (key: string) => {
+  return {
+    id: `tabpanel-${key}`,
+    'aria-labelledby': `tab-${key}`,
+  };
+};
+
+interface HouseholdAssessmentTabPanelProps extends TabDefinition {
+  active: boolean;
+  assessmentRole: AssessmentRole.Intake | AssessmentRole.Exit;
+  refetch: () => Promise<any>;
+  nextTab?: string;
+  previousTab?: string;
+  navigateToTab: (t: string) => void;
+  updateTabStatus: (status: AssessmentStatus, tabId: string) => void;
+}
+
+// Memoized to only re-render when props change (shallow compare)
+const HouseholdAssessmentTabPanel = memo(
+  ({
+    active,
+    id,
+    clientName,
+    enrollmentId,
+    assessmentId,
+    client,
+    relationshipToHoH,
+    assessmentRole,
+    nextTab,
+    previousTab,
+    navigateToTab,
+    refetch,
+    updateTabStatus,
+    status,
+  }: HouseholdAssessmentTabPanelProps) => {
+    console.debug('Rendering assessment panel for', clientName);
+    const readyToSubmit = status === AssessmentStatus.ReadyToSubmit;
+
+    const getFormActionProps = useCallback(
+      (assessment: AssessmentFieldsFragment) => {
+        const hasBeenSubmitted = assessment && !assessment.inProgress;
+
+        const config = [];
+        config.push({
+          id: 'readyToSubmit',
+          label: hasBeenSubmitted ? 'Submitted' : 'Ready to Submit',
+          action: 'SAVE',
+          buttonProps: {
+            variant: 'contained',
+            disabled: hasBeenSubmitted || readyToSubmit,
+            startIcon:
+              hasBeenSubmitted || readyToSubmit ? (
+                <CheckIcon fontSize='small' />
+              ) : null,
+          },
+          onSuccess: () => {
+            updateTabStatus(AssessmentStatus.ReadyToSubmit, id);
+            refetch();
+          },
+        });
+
+        if (!hasBeenSubmitted) {
+          config.push({
+            id: 'save',
+            label: 'Save',
+            action: 'SAVE',
+            buttonProps: { variant: 'outlined' },
+            onSuccess: () => {
+              updateTabStatus(AssessmentStatus.Started, id);
+              refetch();
+            },
+          });
+        }
+
+        config.push({
+          id: 'prev',
+          label: hasBeenSubmitted ? 'Previous' : 'Save & Previous',
+          action: hasBeenSubmitted ? 'NAVIGATE' : 'SAVE',
+          rightAlign: true,
+          buttonProps: {
+            disabled: !previousTab,
+            variant: 'outlined',
+            startIcon: <ArrowBackIcon fontSize='small' />,
+          },
+          onSuccess: () => {
+            if (!previousTab) return;
+            if (!hasBeenSubmitted) {
+              updateTabStatus(AssessmentStatus.Started, id);
+              refetch();
+            }
+            navigateToTab(previousTab);
+          },
+        });
+
+        config.push({
+          id: 'next',
+          label: hasBeenSubmitted ? 'Next' : 'Save & Next',
+          action: hasBeenSubmitted ? 'NAVIGATE' : 'SAVE',
+          rightAlign: true,
+          buttonProps: {
+            disabled: !nextTab,
+            variant: 'outlined',
+            endIcon: <ArrowForwardIcon fontSize='small' />,
+          },
+          onSuccess: () => {
+            if (!nextTab) return;
+            if (!hasBeenSubmitted) {
+              updateTabStatus(AssessmentStatus.Started, id);
+              refetch();
+            }
+            navigateToTab(nextTab);
+          },
+        });
+
+        return { config };
+      },
+      [
+        navigateToTab,
+        previousTab,
+        nextTab,
+        refetch,
+        readyToSubmit,
+        updateTabStatus,
+        id,
+      ]
+    );
+
+    return (
+      <AlwaysMountedTabPanel
+        active={active}
+        key={id}
+        {...tabPanelA11yProps(id)}
+      >
+        <IndividualAssessment
+          clientName={clientName}
+          client={client}
+          relationshipToHoH={relationshipToHoH}
+          embeddedInWorkflow
+          enrollmentId={enrollmentId}
+          assessmentId={assessmentId}
+          assessmentRole={assessmentRole}
+          getFormActionProps={getFormActionProps}
+          lockIfSubmitted
+        />
+      </AlwaysMountedTabPanel>
+    );
+  }
+);
+
+export default HouseholdAssessmentTabPanel;
--- src/modules/assessments/components/household/HouseholdAssessments.tsx
+++ src/modules/assessments/components/household/HouseholdAssessments.tsx
@@ -1,6 +1,3 @@
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import CheckIcon from '@mui/icons-material/Check';
 import {
   Alert,
   AppBar,
@@ -11,12 +8,10 @@ import {
   Typography,
 } from '@mui/material';
 import { findIndex } from 'lodash-es';
-import { memo, ReactNode, useCallback, useEffect, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import IndividualAssessment from '../IndividualAssessment';
-
-import AlwaysMountedTabPanel from './AlwaysMountedTabPanel';
+import HouseholdAssessmentTabPanel from './HouseholdAssessmentTabPanel';
 import TabLabel from './TabLabel';
 
 import Loading from '@/components/elements/Loading';
@@ -28,7 +23,6 @@ import { clientBriefName, enrollmentName } from '@/modules/hmis/hmisUtil';
 import { useHouseholdMembers } from '@/modules/household/components/useHouseholdMembers';
 import { router } from '@/routes/router';
 import {
-  AssessmentFieldsFragment,
   AssessmentRole,
   EnrollmentFieldsFragment,
   RelationshipToHoH,
@@ -46,13 +40,6 @@ const tabA11yProps = (key: string) => {
   return {
     id: `tab-${key}`,
     'aria-controls': `tabpanel-${key}`,
-  };
-};
-
-const tabPanelA11yProps = (key: string) => {
-  return {
-    id: `tabpanel-${key}`,
-    'aria-labelledby': `tab-${key}`,
   };
 };
 
@@ -79,155 +66,6 @@ export type TabDefinition = {
   isHoh: boolean;
   relationshipToHoH: RelationshipToHoH;
 } & LocalTabState;
-
-interface HouseholdAssessmentTabPanelProps extends TabDefinition {
-  active: boolean;
-  assessmentRole: AssessmentRole.Intake | AssessmentRole.Exit;
-  refetch: () => Promise<any>;
-  nextTab?: string;
-  previousTab?: string;
-  navigateToTab: (t: string) => void;
-  updateTabStatus: (status: AssessmentStatus, tabId: string) => void;
-}
-
-// const shallowCompareIgnoreFunctions = (obj1, obj2) =>
-//   Object.keys(obj1).length === Object.keys(obj2).length &&
-//   Object.keys(obj1).every(
-//     (key) => typeof obj1[key] === 'function' || obj1[key] === obj2[key]
-//   );
-
-// Memoized to only re-render when props change (shallow compare)
-const HouseholdAssessmentTabPanel = memo(
-  ({
-    active,
-    id,
-    clientName,
-    enrollmentId,
-    assessmentId,
-    client,
-    relationshipToHoH,
-    assessmentRole,
-    nextTab,
-    previousTab,
-    navigateToTab,
-    refetch,
-    updateTabStatus,
-    status,
-  }: HouseholdAssessmentTabPanelProps) => {
-    console.debug('Rendering assessment panel for', clientName);
-    const readyToSubmit = status === AssessmentStatus.ReadyToSubmit;
-
-    const getFormActionProps = useCallback(
-      (assessment: AssessmentFieldsFragment) => {
-        const hasBeenSubmitted = assessment && !assessment.inProgress;
-
-        const config = [];
-        config.push({
-          id: 'readyToSubmit',
-          label: hasBeenSubmitted ? 'Submitted' : 'Ready to Submit',
-          action: 'SAVE',
-          buttonProps: {
-            variant: 'contained',
-            disabled: hasBeenSubmitted || readyToSubmit,
-            startIcon:
-              hasBeenSubmitted || readyToSubmit ? (
-                <CheckIcon fontSize='small' />
-              ) : null,
-          },
-          onSuccess: () => {
-            updateTabStatus(AssessmentStatus.ReadyToSubmit, id);
-            refetch();
-          },
-        });
-
-        if (!hasBeenSubmitted) {
-          config.push({
-            id: 'save',
-            label: 'Save',
-            action: 'SAVE',
-            buttonProps: { variant: 'outlined' },
-            onSuccess: () => {
-              updateTabStatus(AssessmentStatus.Started, id);
-              refetch();
-            },
-          });
-        }
-
-        config.push({
-          id: 'prev',
-          label: hasBeenSubmitted ? 'Previous' : 'Save & Previous',
-          action: hasBeenSubmitted ? 'NAVIGATE' : 'SAVE',
-          rightAlign: true,
-          buttonProps: {
-            disabled: !previousTab,
-            variant: 'outlined',
-            startIcon: <ArrowBackIcon fontSize='small' />,
-          },
-          onSuccess: () => {
-            if (!previousTab) return;
-            if (!hasBeenSubmitted) {
-              updateTabStatus(AssessmentStatus.Started, id);
-              refetch();
-            }
-            navigateToTab(previousTab);
-          },
-        });
-
-        config.push({
-          id: 'next',
-          label: hasBeenSubmitted ? 'Next' : 'Save & Next',
-          action: hasBeenSubmitted ? 'NAVIGATE' : 'SAVE',
-          rightAlign: true,
-          buttonProps: {
-            disabled: !nextTab,
-            variant: 'outlined',
-            endIcon: <ArrowForwardIcon fontSize='small' />,
-          },
-          onSuccess: () => {
-            if (!nextTab) return;
-            if (!hasBeenSubmitted) {
-              updateTabStatus(AssessmentStatus.Started, id);
-              refetch();
-            }
-            navigateToTab(nextTab);
-          },
-        });
-
-        return { config };
-      },
-      [
-        navigateToTab,
-        previousTab,
-        nextTab,
-        refetch,
-        readyToSubmit,
-        updateTabStatus,
-        id,
-      ]
-    );
-
-    return (
-      <AlwaysMountedTabPanel
-        active={active}
-        key={id}
-        {...tabPanelA11yProps(id)}
-      >
-        <IndividualAssessment
-          clientName={clientName}
-          client={client}
-          relationshipToHoH={relationshipToHoH}
-          embeddedInWorkflow
-          enrollmentId={enrollmentId}
-          assessmentId={assessmentId}
-          assessmentRole={assessmentRole}
-          getFormActionProps={getFormActionProps}
-          lockIfSubmitted
-        />
-      </AlwaysMountedTabPanel>
-    );
-  }
-  // shallowCompareIgnoreFunctions
-);
 
 const HouseholdAssessments = ({ type, title, enrollment }: Props) => {
   const [householdMembers, { loading, error, refetch, networkStatus }] =
