8e3b2da1dbf7d951fc1564e599d5bea5809c8313
Gig Ashton <gig@greenriver.org>
2023-02-17 11:00:07 -0500

    Make ts happy

--- src/utils/generateSafePath.tsx
+++ src/utils/generateSafePath.tsx
@@ -5,16 +5,17 @@ import IdEncoder from '@/modules/hmis/IdEncoder';
 export const isIdParam = (key: string) => key.match(/Id$/);
 
 const generateSafePath: typeof generatePath = (basePath, params) => {
-  const safeParams = { ...params } as NonNullable<typeof params>;
+  const safeParams: Record<string, string | null> = { ...params };
   type Key = keyof NonNullable<typeof params>;
 
   for (const key in params) {
     const param = params[key as Key];
-    if (isIdParam(key) && param)
-      safeParams[key as Key] = IdEncoder.encode(param);
+    if (isIdParam(key) && param) {
+      safeParams[key] = IdEncoder.encode(param);
+    }
   }
 
-  return generatePath(basePath, safeParams);
+  return generatePath(basePath, safeParams as typeof params);
 };
 
 export default generateSafePath;
