db1703e33ece49f9da535e1ce71cb30408892729
zachcmp <zparks88@gmail.com>
2023-04-10 11:02:45 -0400

    Handle info group

--- src/modules/form/components/viewable/DynamicViewGroup.tsx
+++ src/modules/form/components/viewable/DynamicViewGroup.tsx
@@ -1,18 +1,16 @@
+import { Box } from '@mui/system';
+
 import { ViewGroupItemComponentProps } from '../../types';
 import DisabilityTable from '../group/DisabilityTable';
-// import DisabilityTable from './group/DisabilityTable';
 import HorizontalGroup from '../group/HorizontalGroup';
 import InputGroup from '../group/InputGroup';
+import QuestionGroup from '../group/QuestionGroup';
 
 import ViewCard from './group/ViewCard';
-
-// import QuestionGroup from './group/QuestionGroup';
 
 import { Component } from '@/types/gqlTypes';
 
 const DynamicViewGroup = (props: ViewGroupItemComponentProps) => {
-  // TODO: ADD MORE GROUPS!
-
   // Always render top-level groups as cards
   if (props.nestingLevel === 0 && !props.item.component) {
     return (
@@ -38,24 +36,19 @@ const DynamicViewGroup = (props: ViewGroupItemComponentProps) => {
       return <DisabilityTable key={props.item.linkId} {...props} />;
     case Component.HorizontalGroup:
       return <HorizontalGroup key={props.item.linkId} {...props} />;
-    //   case Component.InfoGroup:
-    //     return (
-    //       <Box
-    //         sx={{
-    //           backgroundColor: (theme) => theme.palette.grey[100],
-    //           // borderLeft: (theme) => `1px solid ${theme.palette.grey[300]}`,
-    //           // backgroundColor: 'rgb(255,255,224, 0.8)',
-    //           borderRadius: 1,
-    //           // TODO make flexible if we go with this
-    //           width: '415px',
-    //           pb: 1,
-    //           px: 1,
-    //           ml: -1,
-    //         }}
-    //       >
-    //         <QuestionGroup key={props.item.linkId} {...props} />
-    //       </Box>
-    //     );
+    case Component.InfoGroup:
+      return (
+        <Box
+          sx={{
+            backgroundColor: (theme) => theme.palette.grey[100],
+            borderRadius: 1,
+            p: 1,
+            ml: -1,
+          }}
+        >
+          <QuestionGroup key={props.item.linkId} {...props} />
+        </Box>
+      );
     default:
       return (
         <ViewCard
