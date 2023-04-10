e60b3a110403a0d93768e799027a5bf6bd101748
zachcmp <zparks88@gmail.com>
2023-04-10 14:46:58 -0400

    Finish up assessment view page

--- src/components/dashboard/enrollments/ViewAssessmentPage.tsx
+++ src/components/dashboard/enrollments/ViewAssessmentPage.tsx
@@ -1,16 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Typography } from '@mui/material';
-import { useOutletContext } from 'react-router-dom';
+import EditIcon from '@mui/icons-material/Edit';
+import { Box, Button, Grid, Paper, Typography } from '@mui/material';
+import { ReactNode } from 'react';
+import { useOutletContext, Link as ReactRouterLink } from 'react-router-dom';
 
+import Loading from '@/components/elements/Loading';
+import {
+  CONTEXT_HEADER_HEIGHT,
+  STICKY_BAR_HEIGHT,
+} from '@/components/layout/layoutConstants';
 import { DashboardContext } from '@/components/pages/ClientDashboard';
 import useSafeParams from '@/hooks/useSafeParams';
+import AssessmentTitle from '@/modules/assessments/components/AssessmentTitle';
 import { useAssessment } from '@/modules/assessments/components/useAssessment';
+import FormStepper from '@/modules/form/components/FormStepper';
 import DynamicView from '@/modules/form/components/viewable/DynamicView';
 import { createInitialValuesFromSavedValues } from '@/modules/form/util/formUtil';
+import IdDisplay from '@/modules/hmis/components/IdDisplay';
+import { DashboardRoutes } from '@/routes/routes';
+import { HmisEnums } from '@/types/gqlEnums';
 import { EnrollmentFieldsFragment } from '@/types/gqlTypes';
+import generateSafePath from '@/utils/generateSafePath';
 
-// PLACEHOLDER: implement read-only view of assessments
-const ViewAssessmentPage = () => {
+const ViewAssessmentPage = ({
+  top = STICKY_BAR_HEIGHT + CONTEXT_HEADER_HEIGHT,
+}: {
+  top?: number;
+}) => {
   const { client, enrollment } = useOutletContext<DashboardContext>();
   const { clientId, enrollmentId, assessmentId } = useSafeParams() as {
     clientId: string;
@@ -39,12 +55,67 @@ const ViewAssessmentPage = () => {
     assessment.customForm?.values
   );
 
+  if (dataLoading) return <Loading />;
+
   return (
-    <DynamicView
-      values={values}
-      definition={definition.definition}
-      pickListRelationId={enrollment?.project?.id}
-    />
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <AssessmentTitle
+            assessmentTitle={assessmentTitle}
+            actions={
+              <>
+                <Button
+                  color='secondary'
+                  variant='outlined'
+                  startIcon={<EditIcon />}
+                  component={ReactRouterLink}
+                  to={generateSafePath(DashboardRoutes.EDIT_ASSESSMENT, {
+                    enrollmentId,
+                    assessmentId,
+                    clientId,
+                  })}
+                >
+                  Edit
+                </Button>
+              </>
+            }
+          />
+        </Grid>
+        <Grid item xs={2.5}>
+          <Box
+            sx={{
+              position: 'sticky',
+              top: top + 16,
+            }}
+          >
+            <Paper sx={{ p: 2 }}>
+              {formRole && (
+                <Typography variant='h6' mb={2}>
+                  {HmisEnums.FormRole[formRole]}
+                </Typography>
+              )}
+              <FormStepper
+                items={definition.definition.item}
+                scrollOffset={top}
+              />
+            </Paper>
+            {import.meta.env.MODE === 'development' && assessment && (
+              <Box sx={{ py: 2, px: 1 }}>
+                <IdDisplay prefix='Assessment' id={assessment.id} />
+              </Box>
+            )}
+          </Box>
+        </Grid>
+        <Grid item xs={9.5}>
+          <DynamicView
+            values={values}
+            definition={definition.definition}
+            pickListRelationId={enrollment?.project?.id}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
