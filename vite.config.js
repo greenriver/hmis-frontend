4a33c9b6e6e9ae54da18db520c49e3eb89dec36e
Gig Ashton <gig@greenriver.org>
2023-07-18 09:43:29 -0400

    Drop log statements when minifying

--- vite.config.js
+++ vite.config.js
@@ -75,6 +75,8 @@ export default defineConfig(({ command, mode }) => {
     esbuild: {
       // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
       logOverride: { 'this-is-undefined-in-esm': 'silent' },
+      // Drop log statements when minifying
+      pure: ['console.log', 'console.info', 'console.debug'],
     },
     build: {
       rollupOptions: {
