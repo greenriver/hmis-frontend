92988c8eb916810692792cf980564bb72ea42bbe
Zach Parks <zparks88@gmail.com>
2023-07-27 17:23:48 -0400

    183269204 add cls records (#385)

--- src/types/gqlObjects.ts
+++ src/types/gqlObjects.ts
@@ -895,6 +895,10 @@ export const HmisObjectSchemas: GqlSchema[] = [
     name: 'CurrentLivingSituation',
     fields: [
       {
+        name: 'clsSubsidyType',
+        type: { kind: 'ENUM', name: 'RentalSubsidyType', ofType: null },
+      },
+      {
         name: 'currentLivingSituation',
         type: {
           kind: 'NON_NULL',
