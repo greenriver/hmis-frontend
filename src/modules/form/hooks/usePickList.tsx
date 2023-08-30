5f460b63c6300aa4fcf12ea5765de3ffe25cb851
Gig Ashton <gig@greenriver.org>
2023-08-30 12:02:47 -0400

    Default to cache-and-network for pick lists

--- src/modules/form/hooks/usePickList.tsx
+++ src/modules/form/hooks/usePickList.tsx
@@ -92,6 +92,7 @@ export function usePickList({
     },
     // Skip if it was already resolve with local enums, or if it's an unrecognized reference
     skip: !!resolved || !isKnownType,
+    fetchPolicy: 'cache-and-network',
     ...fetchOptions,
   });
 
