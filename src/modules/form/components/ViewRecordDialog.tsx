b1c44e93142e5b405f39cbc7e0465668920ae506
Theron Toomey <ttoomey@greenriver.org>
2023-09-25 08:33:05 -0400

    186073727-prevent-modal-close-on-backdrop (#486)

--- src/modules/form/components/ViewRecordDialog.tsx
+++ src/modules/form/components/ViewRecordDialog.tsx
@@ -41,6 +41,7 @@ const ViewRecordDialog = <RecordType extends SubmitFormAllowedTypes>({
       scroll='paper'
       fullWidth
       onClose={onClose}
+      enableBackdropClick
       {...props}
     >
       <DialogTitle>
