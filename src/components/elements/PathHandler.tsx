5294e5056cac8b2aecba0406b90d13f4e415ead0
zachcmp <zparks88@gmail.com>
2023-11-14 17:49:52 -0500

    Add path handler to catch spaced urls

---
+++ src/components/elements/PathHandler.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, useEffect, useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const PathHandler = ({ children }: { children: ReactNode }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const attempts = useRef(0);
+
+  useEffect(() => {
+    const { pathname, hash, search } = location;
+
+    if (attempts.current >= 5) {
+      console.error(`Unable to correct route '${pathname}'`);
+      return;
+    }
+
+    const match = pathname.match(/^(.*)(%20)+$/);
+    if (match) {
+      const [, corrected] = match;
+      attempts.current += 1;
+      navigate({ pathname: corrected, hash, search });
+    }
+  }, [location, navigate]);
+
+  return children;
+};
+
+export default PathHandler;
