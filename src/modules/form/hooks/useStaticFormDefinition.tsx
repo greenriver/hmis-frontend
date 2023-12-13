0d4705b539b6845736e6c750ace9c7e55f628616
Gig <gig@greenriver.org>
2023-12-13 10:50:35 -0500

    StaticForm component (#582)

---
+++ src/modules/form/hooks/useStaticFormDefinition.tsx
@@ -0,0 +1,34 @@
+import { useMemo } from 'react';
+
+import { getItemMap } from '../util/formUtil';
+
+import {
+  FormDefinitionFieldsFragment,
+  StaticFormRole,
+  useGetStaticFormDefinitionQuery,
+} from '@/types/gqlTypes';
+
+const useStaticFormDefinition = (
+  role: StaticFormRole,
+  localDefinition?: FormDefinitionFieldsFragment
+) => {
+  const { data, loading, error } = useGetStaticFormDefinitionQuery({
+    variables: { role },
+    skip: !!localDefinition,
+  });
+  const { formDefinition, itemMap } = useMemo(() => {
+    const formDefinition = localDefinition || data?.staticFormDefinition;
+    if (!formDefinition) return {};
+    return {
+      formDefinition,
+      itemMap: getItemMap(formDefinition.definition, false),
+    };
+  }, [data, localDefinition]);
+
+  if (error)
+    throw new Error(`Failed to fetch form definition for role ${role}`);
+
+  return { formDefinition, itemMap, loading };
+};
+
+export default useStaticFormDefinition;
