83d8cb0384ac515ab4e37bb54171020d6d63b404
zachcmp <zparks88@gmail.com>
2023-12-21 16:40:59 -0500

    Add ability to delete form definitions

--- src/modules/admin/components/forms/FormDefinitionsPage.tsx
+++ src/modules/admin/components/forms/FormDefinitionsPage.tsx
@@ -1,9 +1,11 @@
 import AddIcon from '@mui/icons-material/Add';
 import { Button, Paper } from '@mui/material';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import FormDefinitionTable, { Row } from './FormDefinitionTable';
 import PageTitle from '@/components/layout/PageTitle';
 import { useStaticFormDialog } from '@/modules/form/hooks/useStaticFormDialog';
+import { AdminDashboardRoutes } from '@/routes/routes';
 import {
   CreateFormDefinitionDocument,
   CreateFormDefinitionMutation,
@@ -14,9 +16,13 @@ import {
   UpdateFormDefinitionDocument,
   UpdateFormDefinitionMutation,
 } from '@/types/gqlTypes';
+import { evictQuery } from '@/utils/cacheUtil';
+import { generateSafePath } from '@/utils/pathEncoding';
 
 const FormDefinitionsPage = () => {
   const [selected, setSelected] = useState<Row>();
+  const navigate = useNavigate();
+  // Dialog for updating form definitions
   const { openFormDialog: openEditDialog, renderFormDialog: renderEditDialog } =
     useStaticFormDialog<
       UpdateFormDefinitionMutation,
@@ -33,6 +39,7 @@ const FormDefinitionsPage = () => {
       onCompleted: () => {},
       onClose: () => setSelected(undefined),
     });
+  // Dialog for creating new form definitions
   const {
     openFormDialog: openCreateDialog,
     renderFormDialog: renderCreateDialog,
@@ -44,7 +51,14 @@ const FormDefinitionsPage = () => {
     mutationDocument: CreateFormDefinitionDocument,
     getErrors: (data) => data.createFormDefinition?.errors || [],
     getVariables: (values) => ({ input: values as FormDefinitionInput }),
-    onCompleted: () => {},
+    onCompleted: (data) => {
+      const id = data?.createFormDefinition?.formDefinition?.id;
+      evictQuery('formDefinitions');
+      if (id)
+        navigate(
+          generateSafePath(AdminDashboardRoutes.VIEW_FORM, { formId: id })
+        );
+    },
     onClose: () => setSelected(undefined),
   });
   return (
--- src/modules/admin/components/forms/ViewFormDefinitionPage.tsx
+++ src/modules/admin/components/forms/ViewFormDefinitionPage.tsx
@@ -1,18 +1,23 @@
 import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { Button, Stack } from '@mui/material';
 // eslint-disable-next-line no-restricted-imports
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import FormRuleTable from '../formRules/FormRuleTable';
 import ButtonLink from '@/components/elements/ButtonLink';
 import Loading from '@/components/elements/Loading';
 import TitleCard from '@/components/elements/TitleCard';
 import PageTitle from '@/components/layout/PageTitle';
+import DeleteMutationButton from '@/modules/dataFetching/components/DeleteMutationButton';
 import { useStaticFormDialog } from '@/modules/form/hooks/useStaticFormDialog';
 import { AdminDashboardRoutes } from '@/routes/routes';
 import {
   CreateFormRuleDocument,
   CreateFormRuleMutation,
+  DeleteFormDefinitionDocument,
+  DeleteFormDefinitionMutation,
+  DeleteFormDefinitionMutationVariables,
   FormRuleInput,
   MutationCreateFormRuleArgs,
   StaticFormRole,
@@ -23,19 +28,19 @@ import { generateSafePath } from '@/utils/pathEncoding';
 
 const ViewFormDefinitionPage = () => {
   const { formId } = useParams() as { formId: string };
+  const navigate = useNavigate();
 
   const { data: { formDefinition } = {}, error } =
     useGetFormDefinitionByIdQuery({
       variables: { id: formId },
     });
 
-  // Form dialog for adding a new rules
+  // Form dialog for adding new rules
   const { openFormDialog, renderFormDialog } = useStaticFormDialog<
     CreateFormRuleMutation,
     MutationCreateFormRuleArgs
   >({
     formRole: StaticFormRole.FormRule,
-    // initialValues: selectedRule,
     mutationDocument: CreateFormRuleDocument,
     getErrors: (data) => data.createFormRule?.errors || [],
     getVariables: (values) => ({
@@ -52,13 +57,33 @@ const ViewFormDefinitionPage = () => {
       <PageTitle
         title={formDefinition?.title}
         actions={
-          <ButtonLink
-            to={generateSafePath(AdminDashboardRoutes.EDIT_FORM, { formId })}
-            startIcon={<EditIcon />}
-            variant='outlined'
-          >
-            Edit Definition
-          </ButtonLink>
+          <Stack direction='row' gap={2}>
+            <ButtonLink
+              to={generateSafePath(AdminDashboardRoutes.EDIT_FORM, { formId })}
+              startIcon={<EditIcon />}
+              variant='outlined'
+            >
+              Edit Definition
+            </ButtonLink>
+            <DeleteMutationButton<
+              DeleteFormDefinitionMutation,
+              DeleteFormDefinitionMutationVariables
+            >
+              queryDocument={DeleteFormDefinitionDocument}
+              variables={{ id: formId }}
+              idPath={'deleteFormDefinition.formDefinition.id'}
+              recordName='Form Definition'
+              ButtonProps={{
+                startIcon: <DeleteIcon />,
+              }}
+              onSuccess={() => {
+                evictQuery('formDefinitions');
+                navigate(generateSafePath(AdminDashboardRoutes.FORMS));
+              }}
+            >
+              Delete Definition
+            </DeleteMutationButton>
+          </Stack>
         }
       />
 
