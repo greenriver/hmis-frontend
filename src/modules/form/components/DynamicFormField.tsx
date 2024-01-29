731696d66da8c1c9c1aefd8eed0d4938bcf130eb
zachcmp <zparks88@gmail.com>
2024-01-29 16:28:10 -0500

    Address circular deps

--- src/modules/form/components/DynamicFormField.tsx
+++ src/modules/form/components/DynamicFormField.tsx
@@ -1,4 +1,4 @@
-import { pick } from 'lodash-es';
+import { isEmpty, pick } from 'lodash-es';
 import React, { cloneElement, ReactNode, useCallback } from 'react';
 
 import {
@@ -15,11 +15,11 @@ import {
 import {
   buildCommonInputProps,
   getAllChildLinkIds,
-  renderItemWithWrappers,
   transformSubmitValues,
 } from '../util/formUtil';
 
 import AutofillFormItemWrapper from './AutofillFormItemWrapper';
+import DependentFormItemWrapper from './DependentFormItemWrapper';
 import DynamicField from './DynamicField';
 import DynamicGroup from './DynamicGroup';
 import ValueWrapper from './ValueWrapper';
@@ -30,6 +30,53 @@ import {
   ItemType,
   ServiceDetailType,
 } from '@/types/gqlTypes';
+
+export const renderItemWithWrappers = (
+  renderChild: (disabled?: boolean) => ReactNode,
+  item: FormItem,
+  handlers: FormDefinitionHandlers
+) => {
+  const { disabledDependencyMap, autofillInvertedDependencyMap } = handlers;
+
+  if (item.hidden) return null;
+
+  const hasDependencies =
+    disabledDependencyMap[item.linkId] ||
+    !isEmpty(item.enableWhen) ||
+    item.item?.every((item) => item.enableWhen);
+  const hasAutofill =
+    autofillInvertedDependencyMap[item.linkId] || !isEmpty(item.autofillValues);
+
+  if (hasDependencies && hasAutofill) {
+    return (
+      <DependentFormItemWrapper handlers={handlers} item={item}>
+        {(disabled) => (
+          <AutofillFormItemWrapper handlers={handlers} item={item}>
+            {() => renderChild(disabled)}
+          </AutofillFormItemWrapper>
+        )}
+      </DependentFormItemWrapper>
+    );
+  }
+
+  if (hasDependencies) {
+    return (
+      <DependentFormItemWrapper handlers={handlers} item={item}>
+        {renderChild}
+      </DependentFormItemWrapper>
+    );
+  }
+
+  if (hasAutofill) {
+    return (
+      <AutofillFormItemWrapper handlers={handlers} item={item}>
+        {() => renderChild()}
+      </AutofillFormItemWrapper>
+    );
+  }
+
+  return renderChild();
+};
 
 export interface Props {
   handlers: FormDefinitionHandlers;
