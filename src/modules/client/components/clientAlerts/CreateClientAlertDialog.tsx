7e63ae920a76c740e0053595b128a795f38bd99a
martha <medwards@greenriver.org>
2024-01-30 15:37:45 -0500

    Add reminder at top of create client alert form

--- src/modules/client/components/clientAlerts/CreateClientAlertDialog.tsx
+++ src/modules/client/components/clientAlerts/CreateClientAlertDialog.tsx
@@ -1,7 +1,13 @@
-import { Box, DialogContent, DialogProps, DialogTitle } from '@mui/material';
+import {
+  Alert,
+  Box,
+  DialogContent,
+  DialogProps,
+  DialogTitle,
+} from '@mui/material';
 import CommonDialog from '@/components/elements/CommonDialog';
 import StaticForm from '@/modules/form/components/StaticForm';
-import { clientNameAllParts } from '@/modules/hmis/hmisUtil';
+import { clientBriefName } from '@/modules/hmis/hmisUtil';
 import {
   ClientAlertInput,
   ClientWithAlertFieldsFragment,
@@ -18,11 +24,17 @@ export const CreateClientAlertDialog: React.FC<ClientAlertDialogProps> = ({
   client,
   ...props
 }) => {
+  const clientName = clientBriefName(client);
+
   return (
     <CommonDialog maxWidth='sm' fullWidth {...props}>
-      <DialogTitle>Create Alert for {clientNameAllParts(client)}</DialogTitle>
+      <DialogTitle>Create Alert for {clientName}</DialogTitle>
       <DialogContent>
         <Box mt={2}>
+          <Alert sx={{ mb: 2 }} severity='info'>
+            Reminder: All staff who can view {clientName} will be able to see
+            this alert
+          </Alert>
           <StaticForm<
             CreateClientAlertMutation,
             CreateClientAlertMutationVariables
