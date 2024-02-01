d6771f084cb595e5e4fb9ba8e158aaa2c3ff0887
martha <medwards@greenriver.org>
2024-02-01 10:49:58 -0500

    Style the ClientAlert using a new variant

--- src/modules/client/components/clientAlerts/ClientAlert.tsx
+++ src/modules/client/components/clientAlerts/ClientAlert.tsx
@@ -1,5 +1,4 @@
 import { Alert, AlertTitle, Box, Typography } from '@mui/material';
-import theme from '@/config/theme';
 import {
   parseAndFormatDate,
   parseAndFormatDateTime,
@@ -21,51 +20,16 @@ const ClientAlert: React.FC<ClientAlertProps> = ({ clientAlert }) => {
   const showClientName = clientAlert.showClientName;
   const priority = alert.priority || 'low';
 
-  const priorityColors: { [index: string]: any } = {
-    high: {
-      header: theme.palette.alerts.highPriorityErrorBackground,
-      body: `${theme.palette.error.main}04`,
-      border: `${theme.palette.error.main}50`,
-    },
-    medium: {
-      header: theme.palette.alerts.mediumPriorityHeaderBackground,
-      body: theme.palette.alerts.mediumPriorityBodyBackground,
-      border: theme.palette.alerts.mediumPriorityBorder,
-    },
-    low: {
-      header: theme.palette.alerts.lowPriorityHeaderBackground,
-      body: theme.palette.alerts.lowPriorityBodyBackground,
-      border: theme.palette.alerts.lowPriorityHeaderBackground,
-    },
-  };
-
   return (
-    <Alert
-      sx={{
-        p: 0,
-        color: 'black',
-        backgroundColor: priorityColors[priority].body,
-        borderColor: priorityColors[priority].border,
-        '& .MuiAlert-message': {
-          p: 0,
-          width: '100%',
-        },
-      }}
-      icon={false}
-      variant='outlined'
-    >
+    <Alert icon={false} variant='withHeader' color={priority}>
       <AlertTitle
         sx={{
-          p: 2,
-          m: 0,
-          backgroundColor: priorityColors[priority].header,
-          fontWeight: 'bold',
           textTransform: 'capitalize',
         }}
       >
         {priority} Priority Alert
       </AlertTitle>
-      <Box sx={{ p: 2 }}>
+      <Box className='MuiAlert-body'>
         {showClientName && (
           <Typography variant='body2' sx={{ pb: 1 }}>
             {clientName}
