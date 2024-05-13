d07d6e5cd403f2e18dd76941b071767b2dabfd05
martha <medwards@greenriver.org>
2024-05-13 15:59:00 -0400

    Recurse to create form items for tree structure

--- src/modules/formBuilder/components/formTree/FormTree.tsx
+++ src/modules/formBuilder/components/formTree/FormTree.tsx
@@ -2,34 +2,17 @@ import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 import React, { useMemo } from 'react';
 import Loading from '@/components/elements/Loading';
 import FormTreeItem from '@/modules/formBuilder/components/formTree/FormTreeItem';
+import { getItemsForTree } from '@/modules/formBuilder/components/formTree/formTreeUtil';
 import { FormDefinitionJson } from '@/types/gqlTypes';
 
 interface FormTreeProps {
   definition: FormDefinitionJson;
 }
 const FormTree: React.FC<FormTreeProps> = ({ definition }) => {
-  // TODO - let's discuss the best way to support nesting
-  // MUI tree view component expects the 'item' prop to be named 'children', and doesn't provide an overrider function.
-  // OS contribution to react mui that provides an override? (but maybe there's a reason they don't allow)
-  // rewriting our DSL to use 'children' instead of 'item'? (but this feels like big lift + bad coupling)
-  // keep and clean up the below adapter, expanding it to n layers of nesting (n = 5?) ?
-  // We might need something like this adapter anyway to consolidate down some functional groups, like SSN
-  const definitionForTree = useMemo(() => {
-    if (!definition?.item) return [];
-    return definition.item.map((i) => {
-      return {
-        ...i,
-        children: i.item?.map((j) => {
-          return {
-            ...j,
-            children: j.item?.map((k) => {
-              return { ...k, children: k.item };
-            }),
-          };
-        }),
-      };
-    });
-  }, [definition]);
+  const definitionForTree = useMemo(
+    () => getItemsForTree(definition),
+    [definition]
+  );
 
   if (!definition) return <Loading />;
 
