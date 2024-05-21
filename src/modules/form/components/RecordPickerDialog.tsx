9b7e2fae2f802bbb900f12a7b178e7e39ff96cc4
martha <medwards@greenriver.org>
2024-05-21 11:11:32 -0400

    Revert "Suggestion for a better height fix"
    This reverts commit 974d1332be40a1162f6f0e196003224fb3712855.

--- src/modules/form/components/RecordPickerDialog.tsx
+++ src/modules/form/components/RecordPickerDialog.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 import Button from '@mui/material/Button';
 import { DialogProps } from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -70,9 +70,6 @@ const RecordPickerDialog = ({
     return [...assessmentColumns, ...dataColumns];
   }, [item]);
 
-  // Need to set height on the dialog in order for the scrolling to work
-  const height = `${Math.min(Math.max(columns.length * 60 + 250, 550), 850)}px`;
-
   const hudRoles = [
     AssessmentRole.Intake,
     AssessmentRole.Update,
@@ -105,52 +102,49 @@ const RecordPickerDialog = ({
 
       <DialogContent
         sx={{
-          py: 0,
+          pb: 2,
           my: 0,
-          height,
         }}
       >
         {description}
-        <Box sx={{ height: `calc(${height} - 190px)` }}>
-          <AssessmentsForPopulationTable
-            queryVariables={{ id: clientId, roles: hudRoles }}
-            defaultPageSize={5}
-            columns={columns}
-            nonTablePagination
-            vertical
-            fullHeight
-            tableProps={{
-              size: 'small',
-              stickyHeader: true,
-              width: 'fit-content',
-            }}
-            renderVerticalHeaderCell={(record) => {
-              return (
-                <Stack spacing={2} sx={{ py: 1 }}>
-                  <RelativeDate
-                    dateString={record.assessmentDate}
-                    dateUpdated={record.dateUpdated || undefined}
-                    variant='body2'
-                    textAlign={'center'}
-                    fontWeight={600}
-                    withTooltip
-                    prefix='Assessment Date: '
-                  />
-                  <Button
-                    onClick={() => onSelected(record)}
-                    variant='outlined'
-                    size='small'
-                    sx={{ backgroundColor: 'white' }}
-                    fullWidth
-                    aria-label={`Select record from ${record.assessmentDate}`}
-                  >
-                    Select
-                  </Button>
-                </Stack>
-              );
-            }}
-          />
-        </Box>
+        <AssessmentsForPopulationTable
+          queryVariables={{ id: clientId, roles: hudRoles }}
+          defaultPageSize={5}
+          columns={columns}
+          nonTablePagination
+          vertical
+          fullHeight
+          tableProps={{
+            size: 'small',
+            stickyHeader: true,
+            width: 'fit-content',
+          }}
+          renderVerticalHeaderCell={(record) => {
+            return (
+              <Stack spacing={2} sx={{ py: 1 }}>
+                <RelativeDate
+                  dateString={record.assessmentDate}
+                  dateUpdated={record.dateUpdated || undefined}
+                  variant='body2'
+                  textAlign={'center'}
+                  fontWeight={600}
+                  withTooltip
+                  prefix='Assessment Date: '
+                />
+                <Button
+                  onClick={() => onSelected(record)}
+                  variant='outlined'
+                  size='small'
+                  sx={{ backgroundColor: 'white' }}
+                  fullWidth
+                  aria-label={`Select record from ${record.assessmentDate}`}
+                >
+                  Select
+                </Button>
+              </Stack>
+            );
+          }}
+        />
       </DialogContent>
       <DialogActions sx={{ justifyContent: 'center' }}>
         <Button onClick={onCancel} variant='gray'>
