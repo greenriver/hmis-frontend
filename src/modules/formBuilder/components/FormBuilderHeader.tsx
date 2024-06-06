d1b4faafc4a0b50e4ef04a429b6ea018ad4f6cd8
martha <medwards@greenriver.org>
2024-06-06 14:01:33 -0400

    Move form title/role changes into FormBuilderHeader (#798)

--- src/modules/formBuilder/components/FormBuilderHeader.tsx
+++ src/modules/formBuilder/components/FormBuilderHeader.tsx
@@ -5,9 +5,17 @@ import React, { useMemo } from 'react';
 import ButtonTooltipContainer from '@/components/elements/ButtonTooltipContainer';
 import { CommonLabeledTextBlock } from '@/components/elements/CommonLabeledTextBlock';
 import { EditIcon } from '@/components/elements/SemanticIcons';
+import { useStaticFormDialog } from '@/modules/form/hooks/useStaticFormDialog';
 import FormOptionsMenu from '@/modules/formBuilder/components/FormOptionsMenu';
 import { HmisEnums } from '@/types/gqlEnums';
-import { FormDefinitionFieldsForEditorFragment } from '@/types/gqlTypes';
+import {
+  FormDefinitionFieldsForEditorFragment,
+  FormDefinitionInput,
+  MutationUpdateFormDefinitionArgs,
+  StaticFormRole,
+  UpdateFormDefinitionDocument,
+  UpdateFormDefinitionMutation,
+} from '@/types/gqlTypes';
 
 interface FormEditorHeaderProps {
   formDefinition: FormDefinitionFieldsForEditorFragment;
@@ -25,50 +33,70 @@ const FormBuilderHeader: React.FC<FormEditorHeaderProps> = ({
     [formDefinition]
   );
 
+  // Dialog for updating form definitions
+  const { openFormDialog: openEditDialog, renderFormDialog: renderEditDialog } =
+    useStaticFormDialog<
+      UpdateFormDefinitionMutation,
+      MutationUpdateFormDefinitionArgs
+    >({
+      formRole: StaticFormRole.FormDefinition,
+      initialValues: formDefinition || {},
+      localConstants: { definitionId: formDefinition.id },
+      mutationDocument: UpdateFormDefinitionDocument,
+      getErrors: (data) => data.updateFormDefinition?.errors || [],
+      getVariables: (values) => ({
+        input: values as FormDefinitionInput,
+        id: formDefinition.id || '',
+      }),
+    });
+
   return (
-    <Paper sx={{ p: 4 }}>
-      <Stack direction='row' gap={1}>
-        <Typography sx={{ mb: 2 }} variant='h2'>
-          {formDefinition.title}
-        </Typography>
-        <ButtonTooltipContainer title='Edit Title'>
-          <IconButton
-            aria-label='edit title'
-            onClick={() => {}}
-            size='small'
-            sx={{ color: (theme) => theme.palette.links, mt: 0.25 }}
-          >
-            <EditIcon fontSize='inherit' />
-          </IconButton>
-        </ButtonTooltipContainer>
-      </Stack>
-      <Stack direction='row' sx={{ alignItems: 'center' }}>
-        <Grid container columnGap={4} rowGap={2}>
-          <CommonLabeledTextBlock title='Form Identifier'>
-            {formDefinition.identifier}
-          </CommonLabeledTextBlock>
-          <CommonLabeledTextBlock title='Form Type'>
-            {formRole}
-          </CommonLabeledTextBlock>
-          <CommonLabeledTextBlock title='Last Updated'>
-            {lastUpdatedDate}
-          </CommonLabeledTextBlock>
-          <CommonLabeledTextBlock title='Status'>
-            {formDefinition.status}
-          </CommonLabeledTextBlock>
-        </Grid>
-        <Stack direction='row' spacing={2}>
-          <Button
-            variant='outlined'
-            startIcon={<VisibilityIcon />}
-            onClick={onClickPreview}
-          >
-            Preview
-          </Button>
-          <FormOptionsMenu />
+    <>
+      {renderEditDialog({ title: 'Edit Form Details' })}
+      <Paper sx={{ p: 4 }}>
+        <Stack direction='row' gap={1}>
+          <Typography sx={{ mb: 2 }} variant='h2'>
+            {formDefinition.title}
+          </Typography>
+          <ButtonTooltipContainer title='Edit Title'>
+            <IconButton
+              aria-label='edit title'
+              onClick={openEditDialog}
+              size='small'
+              sx={{ color: (theme) => theme.palette.links, mt: 0.25 }}
+            >
+              <EditIcon fontSize='small' />
+            </IconButton>
+          </ButtonTooltipContainer>
         </Stack>
-      </Stack>
-    </Paper>
+        <Stack direction='row' sx={{ alignItems: 'center' }}>
+          <Grid container columnGap={4} rowGap={2}>
+            <CommonLabeledTextBlock title='Form Identifier'>
+              {formDefinition.identifier}
+            </CommonLabeledTextBlock>
+            <CommonLabeledTextBlock title='Form Type'>
+              {formRole}
+            </CommonLabeledTextBlock>
+            <CommonLabeledTextBlock title='Last Updated'>
+              {lastUpdatedDate}
+            </CommonLabeledTextBlock>
+            <CommonLabeledTextBlock title='Status'>
+              {formDefinition.status}
+            </CommonLabeledTextBlock>
+          </Grid>
+          <Stack direction='row' spacing={2}>
+            <Button
+              variant='outlined'
+              startIcon={<VisibilityIcon />}
+              onClick={onClickPreview}
+            >
+              Preview
+            </Button>
+            <FormOptionsMenu />
+          </Stack>
+        </Stack>
+      </Paper>
+    </>
   );
 };
 
