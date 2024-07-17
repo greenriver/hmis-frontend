eca9e231ee36a751fd348313fa4689ec414a45a9
martha <medwards@greenriver.org>
2024-07-17 14:45:13 -0400

    Make collapsible component an accordion

--- src/modules/formBuilder/components/itemEditor/pickLists/PickListOption.tsx
+++ src/modules/formBuilder/components/itemEditor/pickLists/PickListOption.tsx
@@ -1,6 +1,5 @@
-import { ArrowDownward, ArrowUpward } from '@mui/icons-material';
-import { Button, Collapse, Stack } from '@mui/material';
-import React, { useState } from 'react';
+import { Stack } from '@mui/material';
+import SimpleAccordion from '@/components/elements/SimpleAccordion';
 import ControlledCheckbox from '@/modules/form/components/rhf/ControlledCheckbox';
 import ControlledTextInput from '@/modules/form/components/rhf/ControlledTextInput';
 import { FormItemControl } from '@/modules/formBuilder/components/itemEditor/types';
@@ -21,10 +20,8 @@ const PickListOption: React.FC<PickListOptionProps> = ({
   isCodeUnique,
   isInitialSelectedUnique,
 }) => {
-  const [advancedOpen, setAdvancedOpen] = useState(false);
-
   return (
-    <Stack gap={2}>
+    <>
       <ControlledTextInput
         control={control}
         name={`pickListOptions.${index}.code`}
@@ -33,79 +30,82 @@ const PickListOption: React.FC<PickListOptionProps> = ({
         required={true}
         rules={{ validate: (input) => isCodeUnique(input) }}
       />
-      <Button
-        fullWidth
-        variant='text'
-        onClick={() => setAdvancedOpen(!advancedOpen)}
-        endIcon={advancedOpen ? <ArrowUpward /> : <ArrowDownward />}
-      >
-        Advanced
-      </Button>
-
-      <Collapse in={advancedOpen}>
-        <Stack gap={2}>
-          <ControlledTextInput
-            control={control}
-            name={`pickListOptions.${index}.label`}
-            label='Label'
-            helperText='If Label is not provided, Code will be displayed'
-          />
-
-          <ControlledCheckbox
-            name={`pickListOptions.${index}.initialSelected`}
-            control={control}
-            label='Initially selected'
-            helperText='Whether this choice is selected by default. Only one choice may be initially selected.'
-            rules={{ validate: (input) => isInitialSelectedUnique(input) }}
-          />
-
-          {formItemComponent !== Component.Dropdown && (
-            // Helper text is only supported for radio/checkbox, not dropdown options
-            <ControlledTextInput
-              control={control}
-              name={`pickListOptions.${index}.helperText`}
-              label='Helper text'
-              helperText='Helper text (may contain HTML)'
-            />
-          )}
-
-          {formItemComponent === Component.Dropdown && (
-            // Secondary label and group are only supported for dropdowns
-            <>
-              <ControlledTextInput
-                control={control}
-                name={`pickListOptions.${index}.secondaryLabel`}
-                label='Secondary Label'
-                helperText='Secondary label for dropdown items'
-              />
-
-              <Stack direction='row' gap={1}>
+      <SimpleAccordion
+        AccordionProps={{
+          sx: { '&.MuiAccordion-root': { mb: 0, mt: 2 } },
+        }}
+        items={[
+          {
+            key: 'Advanced Options',
+            content: (
+              <>
                 <ControlledTextInput
                   control={control}
-                  name={`pickListOptions.${index}.groupLabel`}
-                  label='Group Label'
-                  helperText='Label for group that this choice belongs to, if grouped'
+                  name={`pickListOptions.${index}.label`}
+                  label='Label'
+                  helperText='If Label is not provided, Code will be displayed'
                 />
+
+                <ControlledCheckbox
+                  name={`pickListOptions.${index}.initialSelected`}
+                  control={control}
+                  label='Initially selected'
+                  helperText='Whether this choice is selected by default. Only one choice may be initially selected.'
+                  rules={{
+                    validate: (input) => isInitialSelectedUnique(input),
+                  }}
+                />
+
+                {formItemComponent !== Component.Dropdown && (
+                  // Helper text is only supported for radio/checkbox, not dropdown options
+                  <ControlledTextInput
+                    control={control}
+                    name={`pickListOptions.${index}.helperText`}
+                    label='Helper text'
+                    helperText='Helper text (may contain HTML)'
+                  />
+                )}
+
+                {formItemComponent === Component.Dropdown && (
+                  // Secondary label and group are only supported for dropdowns
+                  <>
+                    <ControlledTextInput
+                      control={control}
+                      name={`pickListOptions.${index}.secondaryLabel`}
+                      label='Secondary Label'
+                      helperText='Secondary label for dropdown items'
+                    />
+
+                    <Stack direction='row' gap={1}>
+                      <ControlledTextInput
+                        control={control}
+                        name={`pickListOptions.${index}.groupLabel`}
+                        label='Group Label'
+                        helperText='Label for group that this choice belongs to, if grouped'
+                      />
+                      <ControlledTextInput
+                        control={control}
+                        name={`pickListOptions.${index}.groupCode`}
+                        label='Group Code'
+                        helperText='Code for group that this choice belongs to, if grouped'
+                      />
+                    </Stack>
+                  </>
+                )}
+
                 <ControlledTextInput
                   control={control}
-                  name={`pickListOptions.${index}.groupCode`}
-                  label='Group Code'
-                  helperText='Code for group that this choice belongs to, if grouped'
+                  name={`pickListOptions.${index}.numericValue`}
+                  label='Numeric value'
+                  helperText='Numeric value, such as a score, used for comparison in conditional logic'
+                  type='number'
                 />
-              </Stack>
-            </>
-          )}
-
-          <ControlledTextInput
-            control={control}
-            name={`pickListOptions.${index}.numericValue`}
-            label='Numeric value'
-            helperText='Numeric value, such as a score, used for comparison in conditional logic'
-            type='number'
-          />
-        </Stack>
-      </Collapse>
-    </Stack>
+              </>
+            ),
+          },
+        ]}
+      />
+    </>
   );
 };
 
