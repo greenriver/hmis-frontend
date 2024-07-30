ca62ffb00c7fa40d77da95e8427388e7407f71e1
martha <medwards@greenriver.org>
2024-07-30 14:32:11 -0400

    Use consistent view for staff assignment

--- src/modules/enrollment/components/staffAssignment/StaffAssignmentCard.tsx
+++ src/modules/enrollment/components/staffAssignment/StaffAssignmentCard.tsx
@@ -1,18 +1,16 @@
 import EditIcon from '@mui/icons-material/Edit';
-import { Divider, Stack, Typography } from '@mui/material';
-import React, { useMemo, useState } from 'react';
-import {
-  CommonDetailGridContainer,
-  CommonDetailGridItem,
-} from '@/components/elements/CommonDetailGrid';
+import { Divider, Typography } from '@mui/material';
+import React, { useState } from 'react';
 import Loading from '@/components/elements/Loading';
 import TitleCard from '@/components/elements/TitleCard';
 import { useEnrollmentDashboardContext } from '@/components/pages/EnrollmentDashboard';
 import IconButtonContainer from '@/modules/enrollment/components/IconButtonContainer';
 import EditStaffAssignmentDialog from '@/modules/enrollment/components/staffAssignment/EditStaffAssignmentDialog';
+import StaffAssignmentTable, {
+  STAFF_ASSIGNMENT_COLUMNS,
+} from '@/modules/enrollment/components/staffAssignment/StaffAssignmentTable';
 import {
   RelationshipToHoH,
-  StaffAssignmentDetailsFragment,
   useGetHouseholdStaffAssignmentsQuery,
 } from '@/types/gqlTypes';
 
@@ -32,22 +30,6 @@ const StaffAssignmentCard: React.FC<StaffAssignmentCardProps> = ({
   } = useGetHouseholdStaffAssignmentsQuery({
     variables: { id: householdId },
   });
-
-  const assignmentsByType = useMemo(() => {
-    if (!household?.staffAssignments) return {};
-
-    return household.staffAssignments.nodes.reduce(
-      (
-        map: Record<string, StaffAssignmentDetailsFragment[]>,
-        a: StaffAssignmentDetailsFragment
-      ) => {
-        const l = map[a.staffAssignmentType];
-        map[a.staffAssignmentType] = l ? [...l, a] : [a];
-        return map;
-      },
-      {}
-    );
-  }, [household?.staffAssignments]);
 
   const [editDialogOpen, setEditDialogOpen] = useState(false);
 
@@ -86,23 +68,15 @@ const StaffAssignmentCard: React.FC<StaffAssignmentCardProps> = ({
                 No Staff Assigned
               </Typography>
             )}
-            <CommonDetailGridContainer>
-              {Object.entries(assignmentsByType).map(([type, assignments]) => (
-                <CommonDetailGridItem
-                  label={type}
-                  key={type}
-                  sx={{ alignItems: 'top' }}
-                >
-                  <Stack spacing={1}>
-                    {assignments.map((a) => (
-                      <Typography variant='body2' key={a.id}>
-                        {a.user.name}
-                      </Typography>
-                    ))}
-                  </Stack>
-                </CommonDetailGridItem>
-              ))}
-            </CommonDetailGridContainer>
+            <StaffAssignmentTable
+              household={household}
+              columns={[
+                STAFF_ASSIGNMENT_COLUMNS.staffName,
+                STAFF_ASSIGNMENT_COLUMNS.role,
+                STAFF_ASSIGNMENT_COLUMNS.assignmentDate,
+              ]}
+              cardSx={{ border: 'none' }}
+            />
           </>
         )}
       </TitleCard>
--- src/modules/enrollment/components/staffAssignment/StaffAssignmentTable.tsx
+++ src/modules/enrollment/components/staffAssignment/StaffAssignmentTable.tsx
@@ -1,5 +1,6 @@
 import { Card } from '@mui/material';
-import React from 'react';
+import { SxProps } from '@mui/system';
+import React, { useMemo } from 'react';
 import GenericTable from '@/components/elements/table/GenericTable';
 import { ColumnDef } from '@/components/elements/table/types';
 import DeleteMutationButton from '@/modules/dataFetching/components/DeleteMutationButton';
@@ -68,21 +69,30 @@ export const STAFF_ASSIGNMENT_COLUMNS: Record<
 
 interface StaffAssignmentTableProps {
   household: HouseholdWithStaffAssignmentsFragment;
+  columns?: ColumnDef<StaffAssignmentDetailsFragment>[];
+  cardSx?: SxProps;
 }
 
 const StaffAssignmentTable: React.FC<StaffAssignmentTableProps> = ({
   household,
+  columns,
+  cardSx,
 }) => {
+  const defaultColumns = useMemo(
+    () => [
+      STAFF_ASSIGNMENT_COLUMNS.staffName,
+      STAFF_ASSIGNMENT_COLUMNS.role,
+      STAFF_ASSIGNMENT_COLUMNS.assignmentDate,
+      STAFF_ASSIGNMENT_COLUMNS.action,
+    ],
+    []
+  );
+
   return (
-    <Card>
+    <Card sx={cardSx}>
       <GenericTable<StaffAssignmentDetailsFragment>
         rows={household.staffAssignments?.nodes || []}
-        columns={[
-          STAFF_ASSIGNMENT_COLUMNS.staffName,
-          STAFF_ASSIGNMENT_COLUMNS.role,
-          STAFF_ASSIGNMENT_COLUMNS.assignmentDate,
-          STAFF_ASSIGNMENT_COLUMNS.action,
-        ]}
+        columns={columns || defaultColumns}
         noData={'No Staff Assigned'}
         tableProps={{ 'aria-label': 'Current staff assignments' }}
       />
