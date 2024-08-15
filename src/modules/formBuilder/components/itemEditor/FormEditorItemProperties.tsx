00b9f51e5c1cccda556f1328543f439aa63e739c
Gig Ashton <gig@greenriver.org>
2024-08-15 10:16:18 -0400

    Undo

--- src/modules/formBuilder/components/itemEditor/FormEditorItemProperties.tsx
+++ src/modules/formBuilder/components/itemEditor/FormEditorItemProperties.tsx
@@ -181,10 +181,9 @@ const FormEditorItemProperties: React.FC<FormEditorItemPropertiesProps> = ({
   return (
     <form
       // handleSubmit validates input before calling onSubmit
-      onSubmit={handleSubmit((item) => {
-        // console.log(item);
-        return onSave(item as FormItem, initialItem.linkId);
-      })}
+      onSubmit={handleSubmit((item) =>
+        onSave(item as FormItem, initialItem.linkId)
+      )}
     >
       <Box sx={{ p: 2 }}>
         {itemTypeValue !== ItemType.Group && (
