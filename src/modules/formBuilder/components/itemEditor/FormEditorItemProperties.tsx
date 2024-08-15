57ab50e7ba3c1d1d480561f5df1ba535b637c869
Gig Ashton <gig@greenriver.org>
2024-08-15 10:14:57 -0400

    Suggest use ControlledSelect instead of YesNoRadio

--- src/modules/formBuilder/components/itemEditor/FormEditorItemProperties.tsx
+++ src/modules/formBuilder/components/itemEditor/FormEditorItemProperties.tsx
@@ -181,9 +181,10 @@ const FormEditorItemProperties: React.FC<FormEditorItemPropertiesProps> = ({
   return (
     <form
       // handleSubmit validates input before calling onSubmit
-      onSubmit={handleSubmit((item) =>
-        onSave(item as FormItem, initialItem.linkId)
-      )}
+      onSubmit={handleSubmit((item) => {
+        // console.log(item);
+        return onSave(item as FormItem, initialItem.linkId);
+      })}
     >
       <Box sx={{ p: 2 }}>
         {itemTypeValue !== ItemType.Group && (
