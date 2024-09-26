1bc67c52fc4757d26f70f89cf1b0af3d5be904e4
Gig <gig@greenriver.org>
2024-09-26 16:41:14 -0400

    Throw fetch error object directly (#947)

--- src/modules/form/hooks/useFormDefinition.tsx
+++ src/modules/form/hooks/useFormDefinition.tsx
@@ -26,10 +26,7 @@ const useFormDefinition = (
     };
   }, [data?.recordFormDefinition, localDefinition]);
 
-  if (error)
-    throw new Error(
-      `Failed to fetch form definition for role ${queryVariables.role}`
-    );
+  if (error) throw error;
 
   return { formDefinition, itemMap, loading };
 };
