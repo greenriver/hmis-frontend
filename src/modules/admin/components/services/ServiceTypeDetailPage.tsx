4a4ccf0f2dfe0bdcda6660644fc99b28b533bcf3
martha <medwards@greenriver.org>
2024-10-28 11:18:27 -0400

    Support adding new service category in frontend (#969)

--- src/modules/admin/components/services/ServiceTypeDetailPage.tsx
+++ src/modules/admin/components/services/ServiceTypeDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack } from '@mui/material';
+import { Alert, Paper, Stack } from '@mui/material';
 import { useState } from 'react';
 import { generatePath, useNavigate } from 'react-router-dom';
 import { CommonUnstyledList } from '@/components/CommonUnstyledList';
@@ -9,9 +9,10 @@ import RouterLink from '@/components/elements/RouterLink';
 import PageTitle from '@/components/layout/PageTitle';
 import NotFound from '@/components/pages/NotFound';
 import useSafeParams from '@/hooks/useSafeParams';
-import UpdateServiceTypeDialog from '@/modules/admin/components/services/UpdateServiceTypeDialog';
+import ServiceTypeDialog from '@/modules/admin/components/services/ServiceTypeDialog';
 import DeleteMutationButton from '@/modules/dataFetching/components/DeleteMutationButton';
 import { AdminDashboardRoutes } from '@/routes/routes';
+import { HmisEnums } from '@/types/gqlEnums';
 import {
   DeleteServiceTypeDocument,
   DeleteServiceTypeMutation,
@@ -45,7 +46,7 @@ const ServiceTypeDetailPage = () => {
   return (
     <>
       <PageTitle
-        overlineText='Manage Service'
+        overlineText={`${serviceType.hud ? 'View' : 'Manage'} Service`}
         title={serviceType.name}
         endElement={
           !serviceType.hud && (
@@ -79,26 +80,49 @@ const ServiceTypeDetailPage = () => {
         }
       />
       {serviceType && (
-        <UpdateServiceTypeDialog
+        <ServiceTypeDialog
           serviceType={serviceType}
           dialogOpen={updateDialogOpen}
           closeDialog={() => setUpdateDialogOpen(!updateDialogOpen)}
         />
       )}
       <Stack gap={2}>
+        {serviceType.formDefinitions.length === 0 && (
+          <Alert severity='info'>
+            To enable this service in a project, visit{' '}
+            <RouterLink to={generatePath(AdminDashboardRoutes.FORMS)}>
+              Forms
+            </RouterLink>
+            , create or choose the form that will collect this service, and
+            enable it using a Form Rule.
+          </Alert>
+        )}
         <Paper sx={{ p: 2 }}>
           <Stack gap={1}>
-            <CommonLabeledTextBlock title='Service Category'>
-              {serviceType.category}
-            </CommonLabeledTextBlock>
-            <CommonLabeledTextBlock title='Service Type'>
+            <CommonLabeledTextBlock title='Service Type Name'>
               {serviceType.name}
+            </CommonLabeledTextBlock>
+            <CommonLabeledTextBlock title='Service Category'>
+              {serviceType.serviceCategory.name}
             </CommonLabeledTextBlock>
             <CommonLabeledTextBlock title='Supports Bulk Assignment?'>
               {serviceType.supportsBulkAssignment ? 'Yes' : 'No'}
             </CommonLabeledTextBlock>
+
+            {serviceType.hudRecordType && (
+              <CommonLabeledTextBlock title='HUD Record Type'>
+                {HmisEnums.RecordType[serviceType.hudRecordType]}
+              </CommonLabeledTextBlock>
+            )}
+            {serviceType.hudTypeProvided && (
+              <CommonLabeledTextBlock title='HUD Type Provided'>
+                {HmisEnums.ServiceTypeProvided[serviceType.hudTypeProvided]}
+              </CommonLabeledTextBlock>
+            )}
+
             <CommonLabeledTextBlock title='Active Forms'>
               <CommonUnstyledList>
+                {serviceType.formDefinitions.length === 0 && 'No active forms'}
                 {serviceType.formDefinitions.map((formDef) => (
                   <li key={formDef.id}>
                     <RouterLink
