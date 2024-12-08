eab0666df1a4ce91e34585b52cf161a0ec93d60a
Gig Ashton <gig@greenriver.org>
2024-12-08 16:53:12 -0500

    Update button story

--- src/components/elements/Button.stories.tsx
+++ src/components/elements/Button.stories.tsx
@@ -4,6 +4,22 @@ import { Meta, StoryObj } from '@storybook/react';
 
 export default {
   component: Button,
+  // not sure why these won't auto-populate from MUI types
+  argTypes: {
+    color: {
+      options: ['grayscale', 'primary', 'secondary', 'error', 'warning'],
+      control: { type: 'select' },
+    },
+    variant: {
+      control: { type: 'select' },
+      options: ['text', 'outlined', 'contained'],
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+    disabled: { control: 'boolean' },
+  },
 } as Meta<typeof Button>;
 
 type Story = StoryObj<typeof Button>;
@@ -17,8 +33,8 @@ export const Default: Story = {
 export const AllColors: Story = {
   args: {
     children: 'Action',
-    // disabled: true,
     startIcon: <CheckIcon />,
+    // disabled: true,
   },
   render: (props) => (
     <Stack gap={2}>
@@ -27,8 +43,8 @@ export const AllColors: Story = {
           'grayscale',
           'primary',
           'error',
-          'secondary', // not in figma, should not be used?
-          'warning', // not in figma, should not be used?
+          'secondary',
+          'warning',
           'info', // not in figma, should not be used?
           'success', // not in figma, should not be used?
         ] as ButtonProps['color'][]
