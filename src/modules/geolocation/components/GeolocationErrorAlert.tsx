a5fcf09957de1f12709d3e92fee83055b119754d
Gig Ashton <gig@greenriver.org>
2024-12-09 21:28:53 -0500

    Use alert if location request failed

---
+++ src/modules/geolocation/components/GeolocationErrorAlert.tsx
@@ -0,0 +1,47 @@
+import { Alert, Button } from '@mui/material';
+
+import { useState } from 'react';
+
+import { LocationPositionError } from '../hooks/useGeolocation';
+import GeolocationHelpDialog from '@/modules/geolocation/components/GeolocationHelpDialog';
+
+interface Props {
+  error: LocationPositionError;
+}
+
+const GeolocationErrorAlert: React.FC<Props> = ({ error }) => {
+  const [showHelp, setShowHelp] = useState(false);
+
+  return (
+    <>
+      <Alert
+        severity='error'
+        sx={{ my: 0.5 }}
+        icon={false}
+        action={
+          error === LocationPositionError.PERMISSION_DENIED && (
+            <Button
+              variant='gray'
+              // TODO:
+              // variant='outlined'
+              // color='grayscale'
+              onClick={() => setShowHelp(true)}
+            >
+              Learn More
+            </Button>
+          )
+        }
+      >
+        There is no location support on this device, or it is disabled. Please
+        check your settings, then try again.
+      </Alert>
+
+      <GeolocationHelpDialog
+        open={showHelp}
+        onClose={() => setShowHelp(false)}
+      />
+    </>
+  );
+};
+
+export default GeolocationErrorAlert;
--- src/modules/geolocation/components/GeolocationInput.tsx
+++ src/modules/geolocation/components/GeolocationInput.tsx
@@ -3,36 +3,23 @@ import {
   Button,
   FormControl,
   FormHelperText,
-  Link,
   Stack,
   Typography,
 } from '@mui/material';
 
 import React, { useId } from 'react';
 
-import { LocationPositionError, useGeolocation } from '../hooks/useGeolocation';
+import { useGeolocation } from '../hooks/useGeolocation';
 import { ClearIcon, MyLocationIcon } from '@/components/elements/SemanticIcons';
 import { DynamicInputCommonProps } from '@/modules/form/types';
 import BaseMap from '@/modules/geolocation/components/BaseMap';
-import GeolocationHelpDialog from '@/modules/geolocation/components/GeolocationHelpDialog';
+import GeolocationErrorAlert from '@/modules/geolocation/components/GeolocationErrorAlert';
 import { LatLon } from '@/modules/geolocation/types';
 
 interface GeolocationInputProps
   extends Pick<DynamicInputCommonProps, 'label' | 'helperText' | 'disabled'> {
   value?: LatLon | null;
   onChange: (value: LatLon | null) => void;
-}
-
-function getErrorMessage(error: LocationPositionError) {
-  if (error === LocationPositionError.PERMISSION_DENIED) {
-    return 'Get location not allowed';
-  } else if (error === LocationPositionError.POSITION_UNAVAILABLE) {
-    return 'Failed to find location';
-  } else if (error === LocationPositionError.TIMEOUT) {
-    return 'Failed to find location';
-  } else if (error === LocationPositionError.UNKNOWN_ERROR) {
-    return 'Failed to find location';
-  }
 }
 
 const GeolocationInput: React.FC<GeolocationInputProps> = ({
@@ -43,13 +30,14 @@ const GeolocationInput: React.FC<GeolocationInputProps> = ({
   disabled,
 }) => {
   const { requestCoordinates, loading, error } = useGeolocation(onChange);
-  const [showHelp, setShowHelp] = React.useState(false);
-  const mapHeight = 230;
   const labelId = useId();
+  const mapHeight = 230;
+
   return (
     <>
       <FormControl sx={{ width: '100%' }}>
         {label && <div id={labelId}>{label}</div>}
+        {error && <GeolocationErrorAlert error={error} />}
         <Stack direction='row' sx={{ my: 0.5 }} gap={2} alignItems='center'>
           <Button
             onClick={requestCoordinates}
@@ -68,22 +56,6 @@ const GeolocationInput: React.FC<GeolocationInputProps> = ({
             >
               Clear Map
             </Button>
-          )}
-          {error && (
-            <>
-              <Typography color='error' variant='body2'>
-                {getErrorMessage(error)}
-              </Typography>
-              {error === LocationPositionError.PERMISSION_DENIED && (
-                <Link
-                  component='button'
-                  variant='body2'
-                  onClick={() => setShowHelp(true)}
-                >
-                  How do I fix this?
-                </Link>
-              )}
-            </>
           )}
         </Stack>
 
@@ -126,10 +98,6 @@ const GeolocationInput: React.FC<GeolocationInputProps> = ({
           )}
         </Box>
       </FormControl>
-      <GeolocationHelpDialog
-        open={showHelp}
-        onClose={() => setShowHelp(false)}
-      />
     </>
   );
 };
