f44f8a88c42574192924af3a2c0bbdb4bda30a7c
Gig Ashton <gig@greenriver.org>
2024-12-31 12:13:21 -0500

    Revert numeric check

--- src/types/geolocationTypes.ts
+++ src/types/geolocationTypes.ts
@@ -7,9 +7,7 @@ export function isLatLon(value: unknown): value is LatLon {
     typeof value === 'object' &&
     value !== null &&
     'latitude' in value &&
-    'longitude' in value &&
-    typeof value.latitude === 'number' &&
-    typeof value.longitude === 'number'
+    'longitude' in value
   );
 }
 
