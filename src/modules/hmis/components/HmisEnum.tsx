6aec76a6fcd33e200a0c778b8f34c12f4df0cdce
Theron Toomey <ttoomey@greenriver.org>
2025-01-07 18:57:31 +0000

    fix illegal dom nesting

--- src/modules/hmis/components/HmisEnum.tsx
+++ src/modules/hmis/components/HmisEnum.tsx
@@ -29,7 +29,7 @@ interface Props extends TypographyProps {
 const HmisEnum = ({ value, enumMap, noData, ...props }: Props) => {
   const [label, color] = getLabelAndColor(enumMap, value);
   return (
-    <Typography variant='body2' color={color} {...props}>
+    <Typography variant='body2' color={color} component='span' {...props}>
       {label || noData}
     </Typography>
   );
