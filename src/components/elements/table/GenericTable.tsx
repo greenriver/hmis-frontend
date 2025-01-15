737d6edbd08bd4426c3552da0877feef0ef8e85f
martha <medwards@greenriver.org>
2025-01-15 14:15:10 -0500

    Improve a11y of autofill assessment table (#1019)
    * Update Project Enrollment tables
    
    * fix a11y
    
    * More accessibility fixes
    
    * Simplify and resolve todos
    
    * Add storybook story
    
    * Remove errant comment
    
    * Add comments
    
    * Update api of TableRowActions
    
    * Fix issue with fulColSpan
    
    * Implement Table Actions pattern and default columns
    
    * Add random comment
    
    * Add storybook for table actions and fix cde story
    
    * Right-align table row actions
    
    Co-authored-by: Gig <gig@greenriver.org>
    
    * Remove comment
    
    * Replace overrideTableBody with more understandable API
    
    * Implement PR suggestion of moving table row to column def
    
    * Implement PR suggestion of moving table row to column def
    
    * Cleanup and add comments
    
    * add todo
    
    * Port over relevant changes from other PR
    
    * Refactor to use renderCellContents
    
    * Revert out-of-scope change
    
    * Add todo
    
    * Remove circular dependency
    
    * Clean up
    
    * Centralize action column base attrs
    
    * Finish refactor to use column def instead of getTableAction
    
    * Fix bulk services loading
    
    * Provide a visually hidden header when header isnt provided
    
    * Keep existing behavior of individual/household assessment viewing
    
    * PR feedback
    
    * Fix typescript problems
    
    * Fix typescript
    
    * Add accessible text for RelativeDateDisplay
    
    * implement one possible approach for client ID typescript
    
    * Improve a11y of autofill assessment table
    
    * Improve and clean up
    
    * PR feedback
    
    * Revert unneeded
    
    ---------
    
    Co-authored-by: Gig Ashton <gig@greenriver.org>

--- src/components/elements/table/GenericTable.tsx
+++ src/components/elements/table/GenericTable.tsx
@@ -55,6 +55,7 @@ export interface Props<T> {
   actionRow?: ReactNode;
   tableProps?: TableProps;
   vertical?: boolean;
+  verticalHiddenHeader?: string; // For vertically oriented tables, this is the hidden header of the first column
   noHead?: boolean;
   renderVerticalHeaderCell?: RenderFunction<T>;
   rowSx?: (row: T) => SxProps<Theme>;
@@ -80,17 +81,8 @@ const clickableRowStyles = {
   cursor: 'pointer',
 };
 
-const HeaderCell = ({
-  children,
-  sx,
-  padding,
-}: {
-  children: ReactNode;
-  padding?: TableCellProps['padding'];
-  sx?: SxProps<Theme>;
-}) => (
+const HeaderCell = ({ children, sx, ...rest }: TableCellProps) => (
   <TableCell
-    padding={padding}
     sx={{
       borderBottomColor: 'borders.dark',
       borderBottomWidth: 2,
@@ -98,6 +90,7 @@ const HeaderCell = ({
       pb: 1,
       ...sx,
     }}
+    {...rest}
   >
     {children}
   </TableCell>
@@ -115,6 +108,17 @@ export const renderCellContents = <T extends { id: string }>(
   return null;
 };
 
+export const renderHeaderCellContents = <T extends { id: string }>(
+  def: ColumnDef<T>
+) => {
+  return def.header ? (
+    <strong>{def.header}</strong>
+  ) : (
+    // If header isn't provided, add a visually hidden header with the column key for accessibility
+    <Box sx={visuallyHidden}>{def.key}</Box>
+  );
+};
+
 const GenericTable = <T extends { id: string }>({
   rows,
   handleRowClick,
@@ -123,6 +127,7 @@ const GenericTable = <T extends { id: string }>({
   paginated = false,
   loading = false,
   vertical = false,
+  verticalHiddenHeader,
   renderVerticalHeaderCell,
   tablePaginationProps,
   tableContainerProps,
@@ -203,7 +208,12 @@ const GenericTable = <T extends { id: string }>({
     <TableHead sx={{ '.MuiTableCell-head': { verticalAlign: 'bottom' } }}>
       {renderVerticalHeaderCell && (
         <TableRow>
-          <TableCell key='empty' sx={{ backgroundColor: 'background.paper' }} />
+          <TableCell
+            key='empty'
+            sx={{ ...verticalCellSx(0), backgroundColor: 'background.paper' }}
+          >
+            <Box sx={visuallyHidden}>{verticalHiddenHeader}</Box>
+          </TableCell>
           {rows.map((row, idx) => (
             <TableCell key={row.id} sx={verticalCellSx(idx)}>
               {renderVerticalHeaderCell(row)}
@@ -243,12 +253,7 @@ const GenericTable = <T extends { id: string }>({
                 width: def.width,
               }}
             >
-              {def.header ? (
-                <strong>{def.header}</strong>
-              ) : (
-                // If header isn't provided, add a visually hidden header with the column key for accessibility
-                <Box sx={visuallyHidden}>{def.key}</Box>
-              )}
+              {renderHeaderCellContents(def)}
             </HeaderCell>
           ))}
         </TableRow>
@@ -308,9 +313,9 @@ const GenericTable = <T extends { id: string }>({
                   <HeaderCell
                     sx={{ ...verticalCellSx(1), width: '350px' }}
                     key={key(def)}
+                    role='rowheader'
                   >
-                    {' '}
-                    <strong>{def.header}</strong>
+                    {renderHeaderCellContents(def)}
                   </HeaderCell>
                   {rows.map((row, idx) => (
                     <TableCell key={row.id} sx={{ ...verticalCellSx(idx) }}>
