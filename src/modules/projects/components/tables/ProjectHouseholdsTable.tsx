01dd94c3ca72e9663a0e37f2685da10cc29d4a37
martha <medwards@greenriver.org>
2025-01-22 15:20:04 -0500

    Fix typescript from merge

--- src/modules/projects/components/tables/ProjectHouseholdsTable.tsx
+++ src/modules/projects/components/tables/ProjectHouseholdsTable.tsx
@@ -179,6 +179,7 @@ const ProjectHouseholdsTable = ({
     ].map(({ render, ariaLabel, ...rest }) => ({
       ...rest,
       render: () => null,
+      tableCellProps: undefined,
     }));
   }, [staffAssignmentsEnabled]);
 
