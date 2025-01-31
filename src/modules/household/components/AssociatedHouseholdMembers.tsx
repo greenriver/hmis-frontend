3dd8a6ad30a47a8ce7b7ca0639cdc60fa2ff19e7
Gig Ashton <gig@greenriver.org>
2025-01-31 15:00:00 -0500

    Use action menu on AssociatedHouseholdMembers

--- src/modules/household/components/AssociatedHouseholdMembers.tsx
+++ src/modules/household/components/AssociatedHouseholdMembers.tsx
@@ -5,6 +5,7 @@ import { RecentHouseholdMember } from '../types';
 import GenericTable, {
   Props as GenericTableProps,
 } from '@/components/elements/table/GenericTable';
+import { getViewClientMenuItem } from '@/components/elements/table/tableRowActionUtil';
 import { ColumnDef } from '@/components/elements/table/types';
 import { SsnDobShowContextProvider } from '@/modules/client/providers/ClientSsnDobVisibility';
 import { CLIENT_COLUMNS } from '@/modules/search/components/ClientSearch';
@@ -13,7 +14,7 @@ import { ClientFieldsFragment } from '@/types/gqlTypes';
 export const householdMemberColumns: ColumnDef<
   ClientFieldsFragment | RecentHouseholdMember
 >[] = [
-  CLIENT_COLUMNS.linkedNameNewTab,
+  CLIENT_COLUMNS.name,
   { ...CLIENT_COLUMNS.ssn, width: '150px' },
   { ...CLIENT_COLUMNS.dobAge, width: '180px' },
 ];
@@ -22,29 +23,29 @@ interface Props
   extends Omit<GenericTableProps<RecentHouseholdMember>, 'rows' | 'columns'> {
   recentMembers: RecentHouseholdMember[];
   additionalColumns?: ColumnDef<RecentHouseholdMember>[];
-  hideHeaders?: boolean;
 }
 const AssociatedHouseholdMembers = ({
   recentMembers,
   additionalColumns,
-  hideHeaders,
   ...props
 }: Props) => {
   const columns: ColumnDef<RecentHouseholdMember>[] = useMemo(() => {
     return [
-      ...householdMemberColumns.map((c) =>
-        hideHeaders ? { ...c, header: '' } : c
-      ),
+      CLIENT_COLUMNS.name,
+      CLIENT_COLUMNS.age,
+      { header: 'Project', render: (row) => row.projectName },
       ...(additionalColumns || []),
     ];
-  }, [hideHeaders, additionalColumns]);
+  }, [additionalColumns]);
 
   if (recentMembers.length === 0) return null;
+
   return (
     <SsnDobShowContextProvider>
       <GenericTable<RecentHouseholdMember>
         rows={recentMembers || []}
         columns={columns}
+        rowSecondaryActionConfigs={(row) => [getViewClientMenuItem(row.client)]}
         {...props}
       />
     </SsnDobShowContextProvider>
