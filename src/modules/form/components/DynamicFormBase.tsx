099a5d9b991d2ad3226b691849245c003b25bf44
Gig Ashton <gig@greenriver.org>
2025-02-23 16:10:31 -0500

    Add back support for form variant for modals

--- src/modules/form/components/DynamicFormBase.tsx
+++ src/modules/form/components/DynamicFormBase.tsx
@@ -5,7 +5,7 @@ import DynamicFormErrors, { DynamicFormErrorsProps } from './DynamicFormErrors';
 import DynamicFormFields, {
   Props as DynamicFormFieldsProps,
 } from './DynamicFormFields';
-import DynamicFormLayout from './DynamicFormLayout';
+import DynamicFormLayout, { DynamicFormLayoutProps } from './DynamicFormLayout';
 import DynamicFormSaveButtons, {
   DynamicFormSaveButtonsProps,
 } from './DynamicFormSaveButtons';
@@ -13,16 +13,22 @@ import DynamicFormSaveButtons, {
 export interface DynamicFormBaseProps
   extends DynamicFormSaveButtonsProps,
     DynamicFormErrorsProps,
-    DynamicFormFieldsProps {
+    DynamicFormFieldsProps,
+    Pick<DynamicFormLayoutProps, 'variant'> {
   errorRef?: RefObject<HTMLDivElement>;
 }
 
-const DynamicFormBase = ({ errorRef, ...props }: DynamicFormBaseProps) => {
+const DynamicFormBase = ({
+  errorRef,
+  variant,
+  ...props
+}: DynamicFormBaseProps) => {
   return (
     <DynamicFormContainer>
       <DynamicFormLayout
         errors={<DynamicFormErrors {...props} ref={errorRef} />}
         saveButtons={<DynamicFormSaveButtons {...props} />}
+        variant={variant}
       >
         <DynamicFormFields {...props} />
       </DynamicFormLayout>
