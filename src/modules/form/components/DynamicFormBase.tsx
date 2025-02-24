2a547d66a4ade65e1076545c3c61988128af0c71
Gig <gig@greenriver.org>
2025-02-24 09:29:59 -0500

    Fix visual form bug (#1073)
    * Add back support for form variant for modals
    
    * Dont render empty space for errors

--- src/modules/form/components/DynamicFormBase.tsx
+++ src/modules/form/components/DynamicFormBase.tsx
@@ -5,24 +5,39 @@ import DynamicFormErrors, { DynamicFormErrorsProps } from './DynamicFormErrors';
 import DynamicFormFields, {
   Props as DynamicFormFieldsProps,
 } from './DynamicFormFields';
-import DynamicFormLayout from './DynamicFormLayout';
+import DynamicFormLayout, { DynamicFormLayoutProps } from './DynamicFormLayout';
 import DynamicFormSaveButtons, {
   DynamicFormSaveButtonsProps,
 } from './DynamicFormSaveButtons';
+import { hasErrors } from '@/modules/errors/util';
 
 export interface DynamicFormBaseProps
   extends DynamicFormSaveButtonsProps,
     DynamicFormErrorsProps,
-    DynamicFormFieldsProps {
+    DynamicFormFieldsProps,
+    Pick<DynamicFormLayoutProps, 'variant'> {
   errorRef?: RefObject<HTMLDivElement>;
 }
 
-const DynamicFormBase = ({ errorRef, ...props }: DynamicFormBaseProps) => {
+const DynamicFormBase = ({
+  errorRef,
+  variant,
+  ...props
+}: DynamicFormBaseProps) => {
   return (
     <DynamicFormContainer>
       <DynamicFormLayout
-        errors={<DynamicFormErrors {...props} ref={errorRef} />}
+        errors={
+          hasErrors(props.errors) && (
+            <DynamicFormErrors
+              errors={props.errors}
+              errorFilter={props.errorFilter}
+              ref={errorRef}
+            />
+          )
+        }
         saveButtons={<DynamicFormSaveButtons {...props} />}
+        variant={variant}
       >
         <DynamicFormFields {...props} />
       </DynamicFormLayout>
