a4721588bc93b499abe503fc206fb3e18227a6ee
Gig Ashton <gig@greenriver.org>
2025-02-24 11:24:48 -0500

    Address feedback

--- src/modules/household/components/HouseholdMemberTable.tsx
+++ src/modules/household/components/HouseholdMemberTable.tsx
@@ -4,6 +4,7 @@ import { useMemo } from 'react';
 import { externalIdColumn } from '@/components/elements/ExternalIdDisplay';
 import { CheckIcon } from '@/components/elements/SemanticIcons';
 import GenericTable from '@/components/elements/table/GenericTable';
+import { customVisuallyHidden } from '@/config/theme';
 import ClientName from '@/modules/client/components/ClientName';
 import { SsnDobShowContextProvider } from '@/modules/client/providers/ClientSsnDobVisibility';
 import HmisEnum from '@/modules/hmis/components/HmisEnum';
@@ -64,10 +65,18 @@ export const HOUSEHOLD_MEMBER_COLUMNS = {
       </Tooltip>
     ),
     key: 'HoH',
-    render: (hc: HouseholdClientFieldsFragment) =>
-      // Decorative checkmark to indicate HoH. Aria-hidden because Relationship to HoH should be textually represented in another col.
-      hc.enrollment.relationshipToHoH ===
-        RelationshipToHoH.SelfHeadOfHousehold && <CheckIcon aria-hidden />,
+    render: (hc: HouseholdClientFieldsFragment) => {
+      const isHoh =
+        hc.enrollment.relationshipToHoH ===
+        RelationshipToHoH.SelfHeadOfHousehold;
+      const accessibleLabel = `Client ${isHoh ? 'is' : 'is not'} HoH`;
+      return (
+        <>
+          {isHoh && <CheckIcon aria-hidden />}
+          <Box sx={customVisuallyHidden}>{accessibleLabel}</Box>
+        </>
+      );
+    },
   },
   relationshipToHoh: {
     header: 'Relationship to HoH',
