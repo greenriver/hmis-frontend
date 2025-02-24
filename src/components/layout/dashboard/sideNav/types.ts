76ee6bce657ed33dfda9df43edde274941add5d0
Gig <gig@greenriver.org>
2025-02-24 08:26:19 -0500

    Allow nav item to specify active paths (#1072)

--- src/components/layout/dashboard/sideNav/types.ts
+++ src/components/layout/dashboard/sideNav/types.ts
@@ -4,8 +4,9 @@ export interface NavItem<T> {
   icon?: React.ReactElement;
   items?: NavItem<T>[];
   href?: string;
-  path?: string;
-  pathParams?: Record<string, string>;
+  path?: string; // path that this nav item links to
+  pathParams?: Record<string, string>; // params to use for linking to path
+  activePaths?: string[]; // additional paths that this nav item should be active for
   type?: 'topic' | 'category' | 'section';
   hide?: boolean;
   permissions?: (keyof Omit<T, 'id' | '__typename'>)[];
