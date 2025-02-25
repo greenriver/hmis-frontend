af93dfe69dc4d215e9d6806285b106e1dc5cc514
martha <medwards@greenriver.org>
2025-02-25 15:57:48 -0500

    replace snake with camel case

--- src/modules/projects/components/tables/ProjectClientEnrollmentsTable.tsx
+++ src/modules/projects/components/tables/ProjectClientEnrollmentsTable.tsx
@@ -84,7 +84,7 @@ export const HOUSEHOLD_ASSIGNED_STAFF_COL: GenericTableWithDataColumnDef<
     defaultHidden: true,
     queryVariableField: 'includeStaffAssignment',
   },
-  key: 'assigned_staff',
+  key: 'assignedStaff',
   render: renderHouseholdAssignedStaff,
 };
 
