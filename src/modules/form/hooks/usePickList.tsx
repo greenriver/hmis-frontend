59d63c2c98a6f152660b7eb047d5a4acadcd82f2
Gig Ashton <gig@greenriver.org>
2025-03-10 15:17:19 -0400

    Fix initialSelected option list behavior

--- src/modules/form/hooks/usePickList.tsx
+++ src/modules/form/hooks/usePickList.tsx
@@ -1,14 +1,8 @@
 import { QueryHookOptions } from '@apollo/client';
-import { intersectionBy } from 'lodash-es';
 import { useMemo } from 'react';
 
-import {
-  ChangeType,
-  isPickListOption,
-  isPickListOptionArray,
-  PickListArgs,
-} from '../types';
-import { hasMeaningfulValue, resolveOptionList } from '../util/formUtil';
+import { PickListArgs } from '../types';
+import { resolveOptionList } from '../util/formUtil';
 
 import {
   FormItem,
@@ -17,57 +11,6 @@ import {
   PickListType,
   useGetPickListQuery,
 } from '@/types/gqlTypes';
-
-export const getValueFromPickListData = ({
-  item,
-  linkId,
-  value,
-  data,
-  setInitial = true,
-}: {
-  item: FormItem;
-  linkId: string;
-  value: any;
-  data: GetPickListQuery;
-  setInitial?: boolean;
-}) => {
-  if (!data?.pickList) return;
-
-  // If there is no value, look for InitialSelected value and set it
-  if (!hasMeaningfulValue(value)) {
-    // ...Except if we tell it not to
-    if (!setInitial) return;
-
-    const initial = item.repeats
-      ? data.pickList.filter((o) => o.initialSelected)
-      : data.pickList.find((o) => o.initialSelected);
-    if (initial) {
-      return { linkId, value: initial, type: ChangeType.System };
-    }
-    return;
-  }
-
-  // Try to find the "full" option (including label) for this value from the pick list
-  let fullOption;
-  if (isPickListOption(value)) {
-    fullOption = data.pickList.find((o) => o.code === value.code);
-  } else if (isPickListOptionArray(value)) {
-    fullOption = intersectionBy(data.pickList, value, 'code');
-  }
-
-  if (fullOption) {
-    // Update the value so that it has the full option.
-    // This is ONLY NECESSARY BECAUSE some forms have conditional logic based on the "group_code"
-    // on the PickListOption, which would be null to start.
-    return { linkId, value: fullOption, type: ChangeType.System };
-  } else {
-    console.warn(
-      `Selected value '${JSON.stringify(
-        value
-      )}' is not present in option list '${item.pickListReference}'`
-    );
-  }
-};
 
 export function usePickList({
   item,
---
+++ src/modules/form/util/formUtil.getEnrichedValueForChoiceItem.test.ts
@@ -0,0 +1,152 @@
+import { describe, expect, it } from 'vitest';
+import { getEnrichedValueForChoiceItem } from './formUtil';
+
+import { FormItem, ItemType, PickListType } from '@/types/gqlTypes';
+
+const remotePickListMap = {
+  [PickListType.Coc]: [
+    { code: 'ma', label: 'MA-100' },
+    { code: 'ca', label: 'CA-100' },
+  ],
+  [PickListType.AvailableUnitsForEnrollment]: [
+    { code: 'one', label: 'One (default)', initialSelected: true },
+    { code: 'two', label: 'Two' },
+  ],
+};
+const handleError = (message: string) => {
+  throw new Error(message);
+};
+
+const enrich = (item: FormItem, value: any) =>
+  getEnrichedValueForChoiceItem({
+    item,
+    remotePickListMap,
+    defaultValue: value,
+    handleError,
+  });
+
+describe('getEnrichedValueForChoiceItem', () => {
+  describe('single-select choice item', () => {
+    it('enriches value with local pick list reference', () => {
+      const item = {
+        linkId: 'foo',
+        type: ItemType.Choice,
+        pickListReference: 'NoYesMissing',
+      };
+
+      ['YES', { code: 'YES' }].forEach((defaultValue) => {
+        const result = enrich(item, defaultValue);
+        expect(result).toEqual({ code: 'YES', label: 'Yes' });
+      });
+      expect(enrich(item, undefined)).toEqual(undefined);
+    });
+
+    it('enriches value with remote pick list reference', () => {
+      const item = {
+        linkId: 'foo',
+        type: ItemType.Choice,
+        pickListReference: PickListType.Coc,
+      };
+
+      ['ma', { code: 'ma' }].forEach((defaultValue) => {
+        const result = enrich(item, defaultValue);
+        expect(result).toEqual({ code: 'ma', label: 'MA-100' });
+      });
+      expect(enrich(item, undefined)).toEqual(undefined);
+    });
+
+    it('enriches value with remote pick list reference and initialSelected option', () => {
+      const item = {
+        linkId: 'foo',
+        type: ItemType.Choice,
+        pickListReference: PickListType.AvailableUnitsForEnrollment,
+      };
+
+      // null value is enriched to initialSelected option
+      const initial = remotePickListMap[
+        PickListType.AvailableUnitsForEnrollment
+      ].find((o) => o.initialSelected);
+      expect(enrich(item, undefined)).toEqual(initial);
+      expect(enrich(item, null)).toEqual(initial);
+    });
+
+    it('enriches value with local pickListOptions', () => {
+      const opt = { code: 'opt', label: 'Label' };
+      const item = {
+        linkId: 'foo',
+        type: ItemType.Choice,
+        pickListOptions: [opt],
+      };
+
+      [opt.code, { code: 'opt' }].forEach((defaultValue) => {
+        const result = enrich(item, defaultValue);
+        expect(result).toEqual(opt);
+      });
+
+      expect(enrich(item, undefined)).toEqual(undefined);
+    });
+
+    it('enriches value with local pickListOptions and initialSelected option', () => {
+      const opt1 = { code: 'opt', label: 'Label' };
+      const opt2 = { code: 'opt2', label: 'Label', initialSelected: true };
+      const item = {
+        linkId: 'foo',
+        type: ItemType.Choice,
+        pickListOptions: [opt1, opt2],
+      };
+
+      // null value is enriched to initialSelected option
+      expect(enrich(item, undefined)).toEqual(opt2);
+      expect(enrich(item, null)).toEqual(opt2);
+
+      // default val is still enriched as expected
+      [opt1.code, { code: opt1.code }].forEach((defaultValue) => {
+        const result = enrich(item, defaultValue);
+        expect(result).toEqual(opt1);
+      });
+    });
+  });
+
+  describe('multi-select choice item', () => {
+    it('enriches value with local pick list reference', () => {
+      const item = {
+        linkId: 'foo',
+        type: ItemType.Choice,
+        pickListReference: 'NoYesMissing',
+        repeats: true,
+      };
+
+      expect(enrich(item, ['YES'])).toEqual([{ code: 'YES', label: 'Yes' }]);
+      expect(enrich(item, ['YES', 'NO'])).toEqual([
+        {
+          code: 'YES',
+          label: 'Yes',
+        },
+        {
+          code: 'NO',
+          label: 'No',
+        },
+      ]);
+    });
+
+    it('enriches value with local pickListOptions and multiple initialSelected options', () => {
+      const opt1 = { code: 'opt', label: 'Label' };
+      const opt2 = { code: 'opt2', label: 'Label', initialSelected: true };
+      const opt3 = { code: 'opt3', label: 'Label', initialSelected: true };
+      const item = {
+        linkId: 'foo',
+        type: ItemType.Choice,
+        pickListOptions: [opt1, opt2, opt3],
+        repeats: true,
+      };
+
+      // null value is enriched to initialSelected option
+      expect(enrich(item, undefined)).toEqual([opt2, opt3]);
+      expect(enrich(item, null)).toEqual([opt2, opt3]);
+      expect(enrich(item, [])).toEqual([opt2, opt3]);
+
+      // default val is still enriched as expected
+      expect(enrich(item, [opt1.code])).toEqual([opt1]);
+    });
+  });
+});
