f31305ccac6797394d2dd09178c10ed6b494bc08
martha <medwards@greenriver.org>
2025-03-20 17:57:01 -0400

    Add simple story for CommonTabs

---
+++ src/components/elements/CommonTabs.stories.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@mui/material';
+import { Meta, StoryObj } from '@storybook/react';
+import CommonTabs from '@/components/elements/CommonTabs';
+
+export default {
+  component: CommonTabs,
+} as Meta<typeof CommonTabs>;
+
+type Story = StoryObj<typeof CommonTabs>;
+export const Default: Story = {
+  args: {
+    tabDefinitions: [
+      {
+        title: 'First tab',
+        contents: (
+          <>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </>
+        ),
+      },
+      {
+        title: 'Second Tab',
+        contents: <Button>Click here</Button>,
+      },
+    ],
+  },
+};
