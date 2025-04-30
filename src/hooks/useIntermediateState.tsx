37c1c250bc91839be148a9d1d74d04f9ac3458b2
martha <medwards@greenriver.org>
2025-04-30 08:28:59 -0400

    Use usePrevious instead of custom hook

--- src/hooks/useIntermediateState.tsx
+++ src/hooks/useIntermediateState.tsx
@@ -1,21 +1,6 @@
 import { isEqual } from 'lodash-es';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-
-// A custom alternative to useEffect that uses deep comparison to compare
-// the effect dependencies (unlike react's useEffect, which shallow compares).
-const useCustomEffect = (effect: React.EffectCallback, dependencies: any[]) => {
-  const previousDepsRef = useRef<any[]>();
-
-  useEffect(() => {
-    if (
-      !previousDepsRef.current ||
-      !isEqual(previousDepsRef.current, dependencies)
-    ) {
-      effect();
-    }
-    previousDepsRef.current = dependencies;
-  }, [dependencies, effect]);
-};
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import usePrevious from '@/hooks/usePrevious';
 
 const useIntermediateState = <T,>(value: T, defaultValue: T = value) => {
   const [state, setState] = useState<T>(value);
@@ -23,11 +8,17 @@ const useIntermediateState = <T,>(value: T, defaultValue: T = value) => {
   const reset = useCallback(() => setState(defaultValue), [defaultValue]);
   const cancel = useCallback(() => setState(value), [value]);
 
-  // Synchronize the internal state with the external value when it changes.
-  // Example: This handles cases like the user navigating with the browser's back/forward buttons.
-  // (It isn't straightforward to lift state up in order to avoid this effect,
-  // since the state needs to be both independently updateable and derived from the external value.)
-  useCustomEffect(() => setState(value), [value]);
+  const previousValueArg = usePrevious(value);
+  useEffect(() => {
+    // Synchronize the internal state with the external value when it changes.
+    // Deep-equal check since `value` is an object.
+    // Example: This handles cases like the user navigating with the browser's back/forward buttons.
+    // (It isn't straightforward to lift state up in order to avoid this effect,
+    // since the state needs to be both independently updateable and derived from the external value.)
+    if (!isEqual(previousValueArg, value)) {
+      setState(value);
+    }
+  }, [previousValueArg, value]);
 
   return useMemo(
     () => ({
