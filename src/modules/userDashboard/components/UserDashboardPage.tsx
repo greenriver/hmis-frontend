ee2c8f8c421a910988a524f3d7c46ecf81a16203
martha <medwards@greenriver.org>
2025-05-27 11:36:35 -0400

    improve pageload

--- src/modules/userDashboard/components/UserDashboardPage.tsx
+++ src/modules/userDashboard/components/UserDashboardPage.tsx
@@ -3,19 +3,13 @@ import Loading from '@/components/elements/Loading';
 import PageContainer from '@/components/layout/PageContainer';
 import NotFound from '@/components/pages/NotFound';
 import useAuth from '@/modules/auth/hooks/useAuth';
-import { useUser } from '@/modules/dataFetching/hooks/useUser';
 import YourClients from '@/modules/staffAssignment/components/YourClients';
 import YourReferrals from '@/modules/userDashboard/components/YourReferrals';
 import { useGetUserDashboardConfigQuery } from '@/types/gqlTypes';
 
 const UserDashboardPage = () => {
-  const { user: { id } = {} } = useAuth();
-  // Session storage doesn't store the user's firstName, so fetch with graphql
-  const { user } = useUser(id);
-
-  const { data, loading, error } = useGetUserDashboardConfigQuery({
-    fetchPolicy: 'cache-first',
-  });
+  const { user: { name } = {} } = useAuth();
+  const { data, loading, error } = useGetUserDashboardConfigQuery();
 
   const showStaffAssignment =
     data?.userDashboard.userDashboardConfig.showStaffAssignment;
@@ -28,7 +22,7 @@ const UserDashboardPage = () => {
   return (
     <PageContainer
       title={'HMIS Dashboard'}
-      overlineText={`${user.firstName} ${user.lastName}`}
+      overlineText={name}
       maxWidth={showStaffAssignment && showReferrals ? 'xl' : 'lg'}
     >
       <Grid container spacing={2}>
