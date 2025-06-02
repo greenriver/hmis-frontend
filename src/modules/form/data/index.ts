7db8750d1784b9acedbff574abeb5d9392dd305b
Gig Ashton <gig@greenriver.org>
2025-06-02 14:24:36 -0400

    Clean up

--- src/modules/form/data/index.ts
+++ src/modules/form/data/index.ts
@@ -1,15 +1,10 @@
 import adminReferralPosting from '@/modules/form/data/adminReferralPosting.json';
 import referralPosting from '@/modules/form/data/referralPosting.json';
 import search from '@/modules/form/data/search.json';
-import units from '@/modules/form/data/units.json';
 import { FormDefinitionJsonFieldsFragment } from '@/types/gqlTypes';
 
 export const SearchFormDefinition: FormDefinitionJsonFieldsFragment =
   JSON.parse(JSON.stringify(search));
-
-export const UnitsDefinition: FormDefinitionJsonFieldsFragment = JSON.parse(
-  JSON.stringify(units)
-);
 
 export const ReferralPostingDefinition: FormDefinitionJsonFieldsFragment =
   JSON.parse(JSON.stringify(referralPosting));
