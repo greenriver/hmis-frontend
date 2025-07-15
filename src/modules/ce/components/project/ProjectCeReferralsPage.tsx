b009539eff27221379ab6d54086ac86048ee2a33
Gig Ashton <gig@greenriver.org>
2025-07-15 10:51:12 -0400

    Add some TODOs for #7321

--- src/modules/ce/components/project/ProjectCeReferralsPage.tsx
+++ src/modules/ce/components/project/ProjectCeReferralsPage.tsx
@@ -11,6 +11,7 @@ const ProjectCeReferralsPage: React.FC = () => {
   const tabDefinitions = useMemo(() => {
     const defs = [];
 
+    // TODO(#7321): only render referrals tab on this page if this project has CE referral feature enabled (either directly or via waitlist)
     if (project.access.canViewReferrals || project.access.canViewOwnReferrals) {
       defs.push({
         title: 'Referrals',
@@ -19,6 +20,7 @@ const ProjectCeReferralsPage: React.FC = () => {
       });
     }
 
+    // TODO(#7321): only render units tab on this page if this project uses waitlist-based referral creation. It doesn't make sense to link
     if (project.access.canViewUnits) {
       defs.push({
         title: 'Available Units',
@@ -26,9 +28,18 @@ const ProjectCeReferralsPage: React.FC = () => {
         contents: <ProjectOpportunitiesTable projectId={project.id} />,
       });
     }
+
+    // TODO(#7321) if project can send direct referrals (or has any sent referrals?), display table of outgoing referrals
+    // {
+    //   title: 'Sent Referrals',
+    //   key: 'sent-referrals',
+    //   contents: null, // Placeholder for future implementation
+    // }
+
     return defs;
   }, [project]);
 
+  // TODO(#7321) if project can send direct referrals, add button linking to a Send Referral page (similar to NewOutgoingReferralPage)
   return (
     <>
       <PageTitle title='Referrals' tabbedPage />
