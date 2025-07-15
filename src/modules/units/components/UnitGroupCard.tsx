a4dd97bee5258006a9e08023263ddb9d384a5ddf
Gig <gig@greenriver.org>
2025-07-15 11:31:47 -0400

    Unit page updates (#1203)
    * Make requested change to show unit totals on non-CE unit page
    
    * Unit group page layout, comment out unfinished work
    
    * Support marking unit available
    
    * Self review
    
    * Address feedback

--- src/modules/units/components/UnitGroupCard.tsx
+++ src/modules/units/components/UnitGroupCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper, Stack, Typography } from '@mui/material';
+import { Divider, Paper, Stack, Typography } from '@mui/material';
 import ButtonLink from '@/components/elements/ButtonLink';
 import UnitUtilizationByUnitType from '@/modules/units/components/UnitUtilizationByUnitType';
 import { ProjectDashboardRoutes } from '@/routes/routes';
@@ -9,54 +9,64 @@ interface Props {
   unitGroup: UnitGroupCapacityFieldsFragment;
   projectId?: string;
   linkToUnitGroup?: boolean;
+  hideTitle?: boolean;
 }
 
 const UnitGroupCard: React.FC<Props> = ({
   unitGroup,
   projectId,
   linkToUnitGroup = false,
+  hideTitle = false,
 }) => {
+  const hasMultipleUnitTypes = unitGroup.unitTypes.length > 1;
   return (
-    <Paper sx={{ px: 2, pt: 1, pb: 2 }}>
-      <Stack
-        justifyContent='space-between'
-        direction='row'
-        alignItems='flex-start'
-      >
-        <div>
-          <Typography component='h2' variant='h5' sx={{ mb: 1 }}>
-            <Typography variant='overline' display='block'>
-              Unit Group
+    <Paper sx={{ px: 2, pt: hideTitle ? 2 : 1, pb: 2 }}>
+      {!hideTitle && (
+        <Stack
+          justifyContent='space-between'
+          direction='row'
+          alignItems='flex-start'
+          sx={{ mb: 1 }}
+        >
+          <div>
+            <Typography component='h2' variant='h5' sx={{ mb: 1 }}>
+              <Typography variant='overline' display='block'>
+                Unit Group
+              </Typography>
+              {unitGroup.name}
             </Typography>
-            {unitGroup.name}
-          </Typography>
-        </div>
-        {linkToUnitGroup && projectId && (
-          <ButtonLink
-            aria-label={`View ${unitGroup.name}`}
-            to={generateSafePath(ProjectDashboardRoutes.UNIT_GROUP, {
-              projectId,
-              unitGroupId: unitGroup.id,
-            })}
-            sx={{ mt: 1 }}
-          >
-            View
-          </ButtonLink>
-        )}
-      </Stack>
+          </div>
+          {linkToUnitGroup && projectId && (
+            <ButtonLink
+              aria-label={`Manage ${unitGroup.name}`}
+              to={generateSafePath(ProjectDashboardRoutes.UNIT_GROUP, {
+                projectId,
+                unitGroupId: unitGroup.id,
+              })}
+              sx={{ mt: 1 }}
+              size='small'
+            >
+              Manage
+            </ButtonLink>
+          )}
+        </Stack>
+      )}
       {unitGroup.capacity > 0 && (
         <>
-          <Stack direction='row' gap={2}>
-            <Typography variant='body2'>
-              <b>Units:</b> {unitGroup.capacity}
-            </Typography>
-            <Typography variant='body2'>
-              <b>Vacancies:</b> {unitGroup.availability}
-            </Typography>
-          </Stack>
-          <Box sx={{ mt: 2 }}>
-            <UnitUtilizationByUnitType unitTypes={unitGroup.unitTypes} />
-          </Box>
+          {hasMultipleUnitTypes && (
+            <>
+              <Stack direction='row' gap={2}>
+                <Typography variant='body2'>
+                  <b>Total Units:</b> {unitGroup.capacity}
+                </Typography>
+                <Typography variant='body2'>
+                  <b>Vacancies:</b> {unitGroup.availability}
+                </Typography>
+              </Stack>
+              <Divider sx={{ my: 2 }} />
+            </>
+          )}
+          <UnitUtilizationByUnitType unitTypes={unitGroup.unitTypes} />
         </>
       )}
       {unitGroup.capacity === 0 && (
