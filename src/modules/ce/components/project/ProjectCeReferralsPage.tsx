bd4889031a8c290999f8a6128c6d83c8ea8900dc
martha <medwards@greenriver.org>
2025-07-24 10:31:17 -0400

    Use 'send' icon

--- src/modules/ce/components/project/ProjectCeReferralsPage.tsx
+++ src/modules/ce/components/project/ProjectCeReferralsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import ButtonLink from '@/components/elements/ButtonLink';
 import CommonTabs from '@/components/elements/CommonTabs';
-import { GoToIcon } from '@/components/elements/SemanticIcons';
+import { SendIcon } from '@/components/elements/SemanticIcons';
 import PageTitle from '@/components/layout/PageTitle';
 import ProjectOpportunitiesTable from '@/modules/ce/components/project/ProjectOpportunitiesTable';
 import ProjectReferralsTable from '@/modules/ce/components/project/ProjectReferralsTable';
@@ -74,7 +74,7 @@ const ProjectCeReferralsPage: React.FC = () => {
     if (showOutgoingReferrals) {
       return (
         <ButtonLink
-          startIcon={<GoToIcon />}
+          startIcon={<SendIcon />}
           to={generateSafePath(ProjectDashboardRoutes.SEND_REFERRAL, {
             projectId: project.id,
           })}
