65fb10ed984b40339b780f15d77b623c7159e008
martha <medwards@greenriver.org>
2025-08-12 11:36:24 -0400

    Add support for rounding to form expressions (#1223)
    * Add support for rounding to form expressions
    
    * Add tests for edge cases

--- src/modules/form/util/expressions/formulaFunctions.ts
+++ src/modules/form/util/expressions/formulaFunctions.ts
@@ -1,4 +1,17 @@
 const formulaFunctions = new Map();
 formulaFunctions.set('ABS', Math.abs);
+
+// Rounding functions
+formulaFunctions.set('ROUND', Math.round);
+formulaFunctions.set('CEIL', Math.ceil);
+formulaFunctions.set('FLOOR', Math.floor);
+
+// Round to specific decimal places
+formulaFunctions.set('ROUNDTO', (value: number, decimals: number) => {
+  if (decimals === undefined || decimals === null) decimals = 0;
+  const factor = Math.pow(10, decimals);
+  return Math.round(value * factor) / factor;
+});
+
 // functions to expose in a formula expression
 export default formulaFunctions;
