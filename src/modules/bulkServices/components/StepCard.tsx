3623759149c1497f065563a1f45bb8d0de03e16a
martha <medwards@greenriver.org>
2026-02-13 15:05:02 +0100

    move StepCardTitle to own file

--- src/modules/bulkServices/components/StepCard.tsx
+++ src/modules/bulkServices/components/StepCard.tsx
@@ -1,42 +1,7 @@
-import { Box, Paper, Typography } from '@mui/material';
-import { Stack, SxProps } from '@mui/system';
+import { Box, Paper } from '@mui/material';
 import React, { ReactNode } from 'react';
+import StepCardTitle from './StepCardTitle';
 import CommonCard from '@/components/elements/CommonCard';
-
-export const StepCardTitle: React.FC<{
-  step: string;
-  title: string;
-  sx?: SxProps;
-  action?: ReactNode;
-  component?: React.ElementType;
-}> = ({ step, title, action, sx, component = 'h2' }) => (
-  <Stack
-    justifyContent='space-between'
-    direction='row'
-    sx={{ width: '100%', p: 2, ...sx }}
-  >
-    <Stack direction='row' gap={2} alignItems='center'>
-      <Box
-        sx={{
-          backgroundColor: 'primary.main',
-          color: 'white',
-          borderRadius: '50%',
-          width: '26px',
-          height: '26px',
-          lineHeight: '26px',
-          textAlign: 'center',
-          fontWeight: 600,
-        }}
-      >
-        {step}
-      </Box>
-      <Typography variant='h4' component={component}>
-        {title}
-      </Typography>
-    </Stack>
-    {action}
-  </Stack>
-);
 
 const StepCard: React.FC<{
   step: string;
