345b1461478c0a452809efa8a46af48152db5e86
martha <medwards@greenriver.org>
2026-06-23 08:48:31 -0400

    Add CE rule warning dialog for affected candidates (#1493)
    * first pass, needs work
    
    * backend review
    
    * update to match designs more closely
    
    * refactor to use RHF and standardize design patterns
    
    * improve validation dialog
    
    * ai review
    
    * Move CardGroup into shared components dir
    
    * refactor fragments to avoid api churn
    
    * update AffectedUnitGroupsTable
    
    * review AI-gen code (wip)
    
    * move around
    
    * revert changes related to warning dialog; split to separate PR
    
    * rm free text editor (split to separate PR)
    
    * rename for clarity
    
    * more self-review
    
    * refactoring
    
    * refactor to reduce complexity
    
    * straighten up
    
    * more cleanup
    
    * match latest designs
    
    * disable removal of last card
    
    * fix bug with loading
    
    * self-review
    
    * Add CE rule warning dialog for affected candidates
    
    * candidates => clients
    
    * Match latest designs
    
    * move files around for better future organization
    
    * move more things around for clearer structure
    
    * update frontend with backend changes
    
    * rename match rules -> eligibility rules
    
    * use rhf required
    
    * update col headers
    
    * update schema

--- src/modules/admin/components/ceMatchRules/editor/CeMatchRuleForm.tsx
+++ src/modules/admin/components/ceMatchRules/editor/CeMatchRuleForm.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 
 import { CeMatchRuleFormValues, newDraftClause } from './ceMatchRuleFormUtil';
 import CeMatchStructuredExpressionBuilder from './CeMatchStructuredExpressionBuilder';
+import CeMatchRuleConfirmationDialog from './impactWarnings/CeMatchRuleConfirmationDialog';
 import LoadingButton from '@/components/elements/LoadingButton';
 import TitleCard from '@/components/elements/TitleCard';
 import ApolloErrorAlert from '@/modules/errors/components/ApolloErrorAlert';
@@ -12,6 +13,7 @@ import {
   emptyErrorState,
   ErrorState,
   hasErrors,
+  hasOnlyWarnings,
   partitionValidations,
 } from '@/modules/errors/util';
 import { getRequiredLabel } from '@/modules/form/components/RequiredLabel';
@@ -92,7 +94,7 @@ const CeMatchRuleForm = () => {
 
   const handleValidSubmit = (
     values: CeMatchRuleFormValues,
-    confirmed = true
+    confirmed = false
   ) => {
     setSaved(false);
 
@@ -103,6 +105,8 @@ const CeMatchRuleForm = () => {
       },
     });
   };
+
+  const showWarningDialog = hasOnlyWarnings(errorState);
 
   return (
     <Stack gap={2}>
@@ -136,7 +140,7 @@ const CeMatchRuleForm = () => {
         <LoadingButton
           loading={loading}
           variant='contained'
-          onClick={handleSubmit((values) => handleValidSubmit(values))}
+          onClick={handleSubmit((values) => handleValidSubmit(values, false))}
         >
           Save Rule
         </LoadingButton>
@@ -144,6 +148,16 @@ const CeMatchRuleForm = () => {
           Discard
         </Button>
       </Stack>
+      {showWarningDialog && (
+        <CeMatchRuleConfirmationDialog
+          errorState={errorState}
+          loading={loading}
+          onCancel={() => setErrorState(emptyErrorState)}
+          onConfirm={() =>
+            handleSubmit((values) => handleValidSubmit(values, true))()
+          }
+        />
+      )}
     </Stack>
   );
 };
